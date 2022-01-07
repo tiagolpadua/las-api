f245cdfa9e8a4fde9e3e2c9291fdf3e4861c9e5c
mateusspp <teu.patricio@hotmail.com>
2022-01-07 19:09:57 -0300

    Segunda Funçao

--- src/funcoes.js
+++ src/funcoes.js
@@ -8,7 +8,9 @@ function saudar(nome) {
 }
 
 // Escreva uma função que receba um nome completo e retorna apenas o primeiro nome: Tiago Lage Payne de Pádua -> Tiago
-function extrairPrimeiroNome() { }
+function extrairPrimeiroNome(nomeCompleto) { 
+    return nomeCompleto.split(" ") [0];
+}
 
 // Escreva uma função que receba uma palavra e torna a primeira letra maiúscula e as outras minúsculas: tIaGo -> Tiago
 function capitalizar() { }
