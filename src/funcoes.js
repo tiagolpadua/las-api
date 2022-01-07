bc24d34650ad102add217d44a6edc07ef259127d
joselazarojunior <j.lazaro.junior@gmail.com>
2022-01-07 19:31:00 -0300

    Função extrair nome

--- src/funcoes.js
+++ src/funcoes.js
@@ -8,10 +8,16 @@ function saudar(nome) {
  }
 
 // Escreva uma função que receba um nome completo e retorna apenas o primeiro nome: Tiago Lage Payne de Pádua -> Tiago
-function extrairPrimeiroNome() { }
+function extrairPrimeiroNome(nomeCompleto) { 
+
+    let nome = nomeCompleto.substring(0, 5);
+    return nome;
+}
 
 // Escreva uma função que receba uma palavra e torna a primeira letra maiúscula e as outras minúsculas: tIaGo -> Tiago
-function capitalizar() { }
+function capitalizar() { 
+  
+}
 
 // Escreva uma função que recebe um preço original e uma categoria de produto e calcula o valor do imposto. Produtos da categoria Alimentação são isentos. Outros produtos tem um imposto de 10%.
 // (30, Alimentação) => 0
