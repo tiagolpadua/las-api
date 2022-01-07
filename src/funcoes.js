b4f4d330a3a5f9aa829462ab0ffe774abaf771d9
ricardo <rosastaricardo84@gmail.com>
2022-01-07 02:42:58 -0300

    teste3 - func captalizar ok

--- src/funcoes.js
+++ src/funcoes.js
@@ -21,7 +21,20 @@ function extrairPrimeiroNome(nome) {
 }
 
 // Escreva uma função que receba uma palavra e torna a primeira letra maiúscula e as outras minúsculas: tIaGo -> Tiago
-function capitalizar() { }
+function capitalizar(palavra) {
+
+    palavra = palavra.trim();
+
+    let palavraFormatada = palavra[0].toUpperCase();
+    
+    for(let i = 1; i < palavra.length ; i++){
+    
+        palavraFormatada += palavra[i].toLowerCase()
+      
+    }
+    
+    return palavraFormatada
+ }
 
 // Escreva uma função que recebe um preço original e uma categoria de produto e calcula o valor do imposto. Produtos da categoria Alimentação são isentos. Outros produtos tem um imposto de 10%.
 // (30, Alimentação) => 0
