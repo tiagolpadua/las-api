238cd65a78ba787cb1a5e0cb5c4521fc659fd19b
Kelly Carvalho <18kellycarvalho@gmail.com>
2022-01-10 20:32:11 -0300

    tests ok

--- src/funcoes.js
+++ src/funcoes.js
@@ -3,28 +3,28 @@
 // =========
 
 // Escreva uma função que receba um nome e retorne uma saudação para este nome: Tiago -> Olá, Tiago
-function saudar(usuario) { 
+function saudar(usuario) {
     return `Olá, ${usuario}`;
 }
 
 // Escreva uma função que receba um nome completo e retorna apenas o primeiro nome: Tiago Lage Payne de Pádua -> Tiago
 function extrairPrimeiroNome(nome) {
-  
+
     return nome.split(" ")[0];
- }
+}
 
 // Escreva uma função que receba uma palavra e torna a primeira letra maiúscula e as outras minúsculas: tIaGo -> Tiago
-function capitalizar(palavra) { 
-    return palavra[0].toUpperCase()+palavra.replace(/^./,"").toLowerCase();
+function capitalizar(palavra) {
+    return palavra[0].toUpperCase() + palavra.replace(/^./, "").toLowerCase();
 }
 
 // Escreva uma função que recebe um preço original e uma categoria de produto e calcula o valor do imposto. Produtos da categoria Alimentação são isentos. Outros produtos tem um imposto de 10%.
 // (30, Alimentação) => 0
 // (10, Bebida) => 1
-function calculaImposto(precoOriginal,categoriaProduto) { 
+function calculaImposto(precoOriginal, categoriaProduto) {
 
-    return categoriaProduto!=="Alimentação"?(precoOriginal*10)/100:0;
- 
+    return categoriaProduto !== "Alimentação" ? (precoOriginal * 10) / 100 : 0;
+
 }
 
 // Escreva uma função que recebe um preço original, uma categoria de produto e um cupom de desconto e calcula o preço com desconto. Se a categoria for Alimentação e o cupom for NULABSSA, deve ser feito um desconto de 50%. Caso contrário, não há nenhum desconto.
@@ -32,12 +32,12 @@ function calculaImposto(precoOriginal,categoriaProduto) {
 // (10, Bebida, NULABSSA) => 10
 // (30, Alimentação, XPTO) => 30
 // (10, Bebida, XPTO) => 10
-function calculaDesconto(precoOriginal,categoriaProduto,cupom) {
+function calculaDesconto(precoOriginal, categoriaProduto, cupom) {
 
-    return categoriaProduto==="Alimentação"&&cupom==="NULABSSA"?precoOriginal-(precoOriginal*50)/100:precoOriginal;
+    return categoriaProduto === "Alimentação" && cupom === "NULABSSA" ? precoOriginal - (precoOriginal * 50) / 100 : precoOriginal;
 
 
- }
+}
 
 // =========
 // Desejável
@@ -49,15 +49,15 @@ function calculaDesconto(precoOriginal,categoriaProduto,cupom) {
 // (fulano, 4) -> fula...
 function truncar(palavra, comprimento) {
 
-    let comprimentoPadrao = comprimento===undefined?5:comprimento;
- 
-    return palavra.length>comprimentoPadrao?palavra.split("").slice(0,comprimentoPadrao).join("")+"...":palavra;
+    let comprimentoPadrao = comprimento === undefined ? 5 : comprimento;
+
+    return palavra.length > comprimentoPadrao ? palavra.split("").slice(0, comprimentoPadrao).join("") + "..." : palavra;
 
 
- 
 
 
- }
+
+}
 
 // Escreva uma função que valida se o texto informado está preenchido e retorna o texto sem espaços antes ou depois.
 // "" -> undefined
@@ -65,8 +65,8 @@ function truncar(palavra, comprimento) {
 // "      Maria " -> "Maria"
 function validaTextoPreenchido(texto) {
 
-    return texto.trim().length>0?texto.trim():undefined;
- }
+    return texto.trim().length > 0 ? texto.trim() : undefined;
+}
 
 // =======
 // Desafio
@@ -75,32 +75,32 @@ function validaTextoPreenchido(texto) {
 // Escreva uma função que valida se a string passada é uma data de nascimento válida, deve retornar um objeto Date sea data for válida ou NaN caso seja inválida.
 // 01/01/2000 -> Ok
 // 99/99/9999 -> NaN
-function validaData(data) { 
-
-    let dataInput = data.replace("/","-");
-
-   
- 
-    let dataParaTratar = new Date(dataInput);
-
-    let dataFormatada = dataParaTratar.toLocaleDateString('pt-BR',{timeZone:'UTC'});
-   
+function validaData(data) {
 
 
-    console.log("data: ",dataParaTratar.getDate())
+    let mes = data.split("/")[1] > 0 && data.split("/")[1] <= 12 ? true : false;
 
-    let dia = dataParaTratar.getDay()>0?true:false;
+    let dia = data.split("/")[0] > 0 && data.split("/")[0] < 31 ? true : false;
 
-    let mes = dataFormatada.getMonth();
+    let ano = data.split("/")[2] > 1970 ? true : false;
 
-    let ano = dataParaTratar.getFullYear()>1970?true:false;
+    return dia && mes && ano ? Date(ano, mes, dia) : NaN;
 
-    //let verificar = dia&&mes&&ano;
 
-   
-console.log(dia, " dia: ",dataParaTratar.getDay()," ", mes, " mes: ",dataParaTratar.getMonth(), " ",ano, " ano: ",dataParaTratar.getFullYear(), " Resultado: ",dia&&mes&&ano);
 
-console.log();
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
