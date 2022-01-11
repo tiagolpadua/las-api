ca95ef86bd75ef2221f4852a53e48ff6e3d14515
Leticia Ribeiro <letsribeiro2@gmail.com>
2022-01-11 17:17:54 -0300

    ready :rocket:

--- src/funcoes.js
+++ src/funcoes.js
@@ -3,25 +3,47 @@
 // =========
 
 // Escreva uma função que receba um nome e retorne uma saudação para este nome: Tiago -> Olá, Tiago
-function saudar() { }
+function saudar(nome) { 
+    return `Olá, ${nome}`; 
+}
 
 // Escreva uma função que receba um nome completo e retorna apenas o primeiro nome: Tiago Lage Payne de Pádua -> Tiago
-function extrairPrimeiroNome() { }
+function extrairPrimeiroNome(fullName) { 
+    let names = fullName.split(' ');
+    return names[0];
+}
 
 // Escreva uma função que receba uma palavra e torna a primeira letra maiúscula e as outras minúsculas: tIaGo -> Tiago
-function capitalizar() { }
+function capitalizar(name) { 
+    let primeiraLetra = name[0].toUpperCase();
+    let restoNome = name.slice(1).toLowerCase();
+    return primeiraLetra + restoNome;
+
+}
 
 // Escreva uma função que recebe um preço original e uma categoria de produto e calcula o valor do imposto. Produtos da categoria Alimentação são isentos. Outros produtos tem um imposto de 10%.
 // (30, Alimentação) => 0
 // (10, Bebida) => 1
-function calculaImposto() { }
+function calculaImposto(preco, categoria) {
+    if (categoria !== "Alimentação") {
+        let imposto = preco * 0.1;
+        return imposto;
+    } else {
+        return 0;
+    }
+ }
 
 // Escreva uma função que recebe um preço original, uma categoria de produto e um cupom de desconto e calcula o preço com desconto. Se a categoria for Alimentação e o cupom for NULABSSA, deve ser feito um desconto de 50%. Caso contrário, não há nenhum desconto.
 // (30, Alimentação, NULABSSA) => 15
 // (10, Bebida, NULABSSA) => 10
 // (30, Alimentação, XPTO) => 30
 // (10, Bebida, XPTO) => 10
-function calculaDesconto() { }
+function calculaDesconto(preco, categoria, cupom) {
+    if (categoria === "Alimentação" && cupom === "NULABSSA") {
+        preco *= 0.5;
+    }
+    return preco;
+ }
 
 // =========
 // Desejável
@@ -31,13 +53,24 @@ function calculaDesconto() { }
 // o valor default do comprimento máximo deve ser 5:
 // (teste, 10) -> teste
 // (fulano, 4) -> fula...
-function truncar() { }
+function truncar(palavra, comprimento = 5) { 
+    if (palavra.length > comprimento) {
+        return palavra.slice(0, comprimento) + "...";
+    } else {
+        return palavra;
+    }
+}
 
 // Escreva uma função que valida se o texto informado está preenchido e retorna o texto sem espaços antes ou depois.
 // "" -> undefined
 // "   " -> undefined
 // "      Maria " -> "Maria"
-function validaTextoPreenchido() { }
+function validaTextoPreenchido(texto) {
+    let novoTexto = texto.trim();
+    if (novoTexto) {
+        return novoTexto;
+    }
+ }
 
 // =======
 // Desafio
@@ -46,6 +79,14 @@ function validaTextoPreenchido() { }
 // Escreva uma função que valida se a string passada é uma data de nascimento válida, deve retornar um objeto Date sea data for válida ou NaN caso seja inválida.
 // 01/01/2000 -> Ok
 // 99/99/9999 -> NaN
-function validaData() { }
+function validaData(data) { 
+    
+    data = new Date (data.split("/").reverse().join("-"));
+
+    if (Date.parse(data)) {
+        return data;
+    }
+    return NaN;
+}
 
 module.exports = { saudar, extrairPrimeiroNome, capitalizar, calculaImposto, calculaDesconto, truncar, validaTextoPreenchido, validaData };
