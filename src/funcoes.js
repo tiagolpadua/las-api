ac467773b542f4fa63c4383f7772b1200282814f
Gilson Junior <55880362+gilsaumjr@users.noreply.github.com>
2022-01-11 20:24:00 -0300

    Essencial e desejável concluídos. Desafio 1 de 2 completo.

--- src/funcoes.js
+++ src/funcoes.js
@@ -85,7 +85,20 @@ function validaTextoPreenchido(texto) {
 // 01/01/2000 -> Ok
 // 99/99/9999 -> NaN
 function validaData(data) { 
-    
+    let dia = +data.substr(0,2);
+    let mes = +data.substr(3,2);
+    let ano = +data.substr(6,4);
+    let dataFormata = new Date(ano,mes,dia);
+    let dataHoje = new Date();
+    if(dataFormata.getFullYear<dataHoje.getFullYear||
+        dataFormata.getFullYear===dataHoje.getFullYear&&dataFormata.getMonth<dataHoje.getMonth||
+        dataFormata.getFullYear===dataHoje.getFullYear&&dataFormata.getMonth===dataHoje.getMonth&&
+        dataFormata.getDate()<dataHoje.getDate()||
+        isNaN(dia)!=true||isNaN(mes)!=true||isNaN(ano)!=true||dataFormata.getDate>31||dataFormata.getMonth>12){
+        return "OK"
+    }else {              
+        return NaN;
+    }
 }
 
 module.exports = { saudar, extrairPrimeiroNome, capitalizar, calculaImposto, calculaDesconto, truncar, validaTextoPreenchido, validaData };
