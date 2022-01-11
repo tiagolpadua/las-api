1c91cef84cd451d07d9d5afa0e2ff02fea754b75
Tauane Sales <taysales6@gmail.com>
2022-01-11 10:28:14 -0300

    Refatora o codigo

--- src/funcoes.js
+++ src/funcoes.js
@@ -9,10 +9,9 @@ function saudar(name) {
 
 // Escreva uma função que clearreceba um nome completo e retorna apenas o primeiro nome: Tiago Lage Payne de Pádua -> Tiago
 function extrairPrimeiroNome(fullName) { 
-   const firstName = fullName.split(' ').shift();
-    return firstName;
+   return firstName = fullName.split(' ').shift();
+    
 }
-//console.log(extrairPrimeiroNome("Tauane Sales"))
 
 // Escreva uma função que receba uma palavra e torna a primeira letra maiúscula e as outras minúsculas: tIaGo -> Tiago
 function capitalizar(word) { 
@@ -20,33 +19,26 @@ function capitalizar(word) {
     sli = capitalized.slice(1, );
     return capitalized[0] + sli.toLowerCase();
 }
-//console.log(capitalizar("tAUANE"));
+
 
 // Escreva uma função que recebe um preço original e uma categoria de produto e calcula o valor do imposto. Produtos da categoria Alimentação são isentos. Outros produtos tem um imposto de 10%.
 // (30, Alimentação) => 0
 // (10, Bebida) => 1
-function calculaImposto(precoOriginal, categoriaProduto) { 
-    if (categoriaProduto != "Alimentação") { 
-        return precoOriginal*10/100; }
-        else{
-            return 0;  }
+
+function calculaImposto(originalPrice, productCategory) { 
+    return productCategory != "Alimentação" ? originalPrice*10/100 : 0;
 }
-//console.log(calculaImposto(10,'Bebida'));
+
 
 // Escreva uma função que recebe um preço original, uma categoria de produto e um cupom de desconto e calcula o preço com desconto. Se a categoria for Alimentação e o cupom for NULABSSA, deve ser feito um desconto de 50%. Caso contrário, não há nenhum desconto.
 // (30, Alimentação, NULABSSA) => 15
 // (10, Bebida, NULABSSA) => 10
 // (30, Alimentação, XPTO) => 30
 // (10, Bebida, XPTO) => 10
-function calculaDesconto(precoOriginal, categoriaProduto, cupom) { 
-    if ((categoriaProduto === "Alimentação") && (cupom==="NULABSSA")) { 
-        return precoOriginal-(precoOriginal*50/100);
-    }
-    else{
-        return precoOriginal;
-    }
+function calculaDesconto(originalPrice, productCategory,discountCoupon) { 
+    return ((productCategory === "Alimentação") && (discountCoupon==="NULABSSA")) ? originalPrice-(originalPrice*50/100) : originalPrice
 }
-//console.log(calculaDesconto(30,"Alimentação","NULABSSA"));
+
 
 // =========
 // Desejável
@@ -57,28 +49,18 @@ function calculaDesconto(precoOriginal, categoriaProduto, cupom) {
 // (teste, 10) -> teste
 // (fulano, 4) -> fula...
 function truncar(word,lengthMax = 5) { 
-    if ((word.length>0) && (word.length>lengthMax)){
-        return word.slice(0, lengthMax)+"...";
-        }
-        else {
-            return word;
-        }
-    }
-console.log(truncar("Tau", 2));
+    return ((word.length>0) && (word.length>lengthMax)) ? word.slice(0, lengthMax)+"..." : word
+}
+    
 
 // Escreva uma função que valida se o texto informado está preenchido e retorna o texto sem espaços antes ou depois.
 // "" -> undefined
 // "   " -> undefined
 // "      Maria " -> "Maria"
 function validaTextoPreenchido(text) { 
-    if ((text.length != "") && (text.trim().length != "")) {
-        return text.trim();
-    }
-       else {
-           return undefined;
-    }
+    return ((text.length != "") && (text.trim().length != "")) ? text.trim() :  undefined;
 }
-console.log(validaTextoPreenchido(" Tauane"));
+
 
 // =======
 // Desafio
@@ -87,8 +69,9 @@ console.log(validaTextoPreenchido(" Tauane"));
 // Escreva uma função que valida se a string passada é uma data de nascimento válida, deve retornar um objeto Date sea data for válida ou NaN caso seja inválida.
 // 01/01/2000 -> Ok
 // 99/99/9999 -> NaN
-function validaData(dataNascimento) {
-
- }
+function validaData(birthDate){
+    var validarData = /^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[1,3-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/;
+    return ((validarData .test(birthDate)) ? "Ok" : NaN);
+}
 
 module.exports = { saudar, extrairPrimeiroNome, capitalizar, calculaImposto, calculaDesconto, truncar, validaTextoPreenchido, validaData };
