b5adea39fbbfa49c4d5a4deb6711822d829de751
JulianaMAN <jmalencarn@gmail.com>
2022-01-10 21:53:09 -0300

    exercício 1

--- src/funcoes.js
+++ src/funcoes.js
@@ -3,7 +3,13 @@
 // =========
 
 // Escreva uma função que receba um nome e retorne uma saudação para este nome: Tiago -> Olá, Tiago
-function saudar() { }
+let nome = "Tiago"
+
+function saudar(nome) { 
+    return `Olá, ${nome}`;
+}
+
+saudar(nome)
 
 // Escreva uma função que receba um nome completo e retorna apenas o primeiro nome: Tiago Lage Payne de Pádua -> Tiago
 function extrairPrimeiroNome() { }
