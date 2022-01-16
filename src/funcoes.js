11c4d77020c65399595f3fa5752a0f608f9eb9d8
Renata Santana <renatasantana114@gmail.com>
2022-01-15 21:45:01 -0300

    Realização do exercício proposto

--- src/funcoes.js
+++ src/funcoes.js
@@ -3,25 +3,48 @@
 // =========
 
 // Escreva uma função que receba um nome e retorne uma saudação para este nome: Tiago -> Olá, Tiago
-function saudar() { }
+function saudar(nome){
+    return ( 'Olá, ' + nome);
+} 
 
+ 
 // Escreva uma função que receba um nome completo e retorna apenas o primeiro nome: Tiago Lage Payne de Pádua -> Tiago
-function extrairPrimeiroNome() { }
+function extrairPrimeiroNome(nome){
+return primeiroNome = nome.split(' ').slice(0, 1).join(' ');
+}
 
 // Escreva uma função que receba uma palavra e torna a primeira letra maiúscula e as outras minúsculas: tIaGo -> Tiago
-function capitalizar() { }
+function capitalizar(nome) {
+    return nome[0].toUpperCase() + nome.slice(1).toLowerCase()
+ }
+
 
 // Escreva uma função que recebe um preço original e uma categoria de produto e calcula o valor do imposto. Produtos da categoria Alimentação são isentos. Outros produtos tem um imposto de 10%.
 // (30, Alimentação) => 0
 // (10, Bebida) => 1
-function calculaImposto() { }
+function calculaImposto(valor, categoria) { 
+if (categoria === "Alimentação"){
+    valorDoImposto = 0;
+} else {
+    valorDoImposto = (valor/100) * 10;
+}
+return valorDoImposto;
+}
 
 // Escreva uma função que recebe um preço original, uma categoria de produto e um cupom de desconto e calcula o preço com desconto. Se a categoria for Alimentação e o cupom for NULABSSA, deve ser feito um desconto de 50%. Caso contrário, não há nenhum desconto.
 // (30, Alimentação, NULABSSA) => 15
 // (10, Bebida, NULABSSA) => 10
 // (30, Alimentação, XPTO) => 30
 // (10, Bebida, XPTO) => 10
-function calculaDesconto() { }
+function calculaDesconto(valor, categoria, cupom) { 
+    if (categoria === 'Alimentação' && cupom === 'NULABSSA'){
+        valorFinal = valor - (valor/100) * 50;
+    }else{
+      valorFinal = valor;
+}
+    return  valorFinal;
+}
+
 
 // =========
 // Desejável
@@ -31,13 +54,25 @@ function calculaDesconto() { }
 // o valor default do comprimento máximo deve ser 5:
 // (teste, 10) -> teste
 // (fulano, 4) -> fula...
-function truncar() { }
+function truncar(palavra, comprimento=5) {
+    if (palavra.length > comprimento){
+        palavra = palavra.substring(0, comprimento) + "...";
+    }
+    return palavra;
+}
 
 // Escreva uma função que valida se o texto informado está preenchido e retorna o texto sem espaços antes ou depois.
 // "" -> undefined
 // "   " -> undefined
 // "      Maria " -> "Maria"
-function validaTextoPreenchido() { }
+function validaTextoPreenchido(texto) {
+    if (texto == false){
+        texto = undefined;
+    } else {
+        texto = texto.trim();
+    }
+    return texto;
+ }
 
 // =======
 // Desafio
