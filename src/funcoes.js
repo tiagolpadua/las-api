c38e188e3f11d5963768cd03a4d49d0161a4456d
Roberto Augusto <roberto.augusto.jr@gmail.com>
2022-01-16 16:26:30 -0300

    Essencial Completo

--- src/funcoes.js
+++ src/funcoes.js
@@ -3,25 +3,43 @@
 // =========
 
 // Escreva uma função que receba um nome e retorne uma saudação para este nome: Tiago -> Olá, Tiago
-function saudar() { }
+function saudar(nome) {
+    return `Olá, ${nome}`;
+}
 
 // Escreva uma função que receba um nome completo e retorna apenas o primeiro nome: Tiago Lage Payne de Pádua -> Tiago
-function extrairPrimeiroNome() { }
+function extrairPrimeiroNome(nomeCompleto) { 
+    primeiroNome=nomeCompleto.split(" ");
+    return primeiroNome[0];
+}
 
 // Escreva uma função que receba uma palavra e torna a primeira letra maiúscula e as outras minúsculas: tIaGo -> Tiago
-function capitalizar() { }
+function capitalizar(primeiraMaiuscula) {
+    return primeiraMaiuscula[0].toUpperCase() + primeiraMaiuscula.slice(1).toLowerCase();
+ }
 
 // Escreva uma função que recebe um preço original e uma categoria de produto e calcula o valor do imposto. Produtos da categoria Alimentação são isentos. Outros produtos tem um imposto de 10%.
 // (30, Alimentação) => 0
 // (10, Bebida) => 1
-function calculaImposto() { }
+function calculaImposto(precoOriginalImposto, categoriaProdutoImposto) {
+    if (categoriaProdutoImposto === "Alimentação"){
+        return 0;
+    }
+    if (categoriaProdutoImpostos === "Bebida"){
+        return precoOriginalImposto*0.10;
+    }
+ }
 
 // Escreva uma função que recebe um preço original, uma categoria de produto e um cupom de desconto e calcula o preço com desconto. Se a categoria for Alimentação e o cupom for NULABSSA, deve ser feito um desconto de 50%. Caso contrário, não há nenhum desconto.
 // (30, Alimentação, NULABSSA) => 15
 // (10, Bebida, NULABSSA) => 10
 // (30, Alimentação, XPTO) => 30
 // (10, Bebida, XPTO) => 10
-function calculaDesconto() { }
+function calculaDesconto(precoOriginalDesconto,categoriaProdutoDesconto, cumpomDesconto) { 
+  if (cumpomDesconto === 'NULABSSA' && categoriaProdutoDesconto === 'Alimentação'){
+        return precoOriginalDesconto-=precoOriginalDesconto*0.5;
+          }else{ return precoOriginalDesconto}
+    }
 
 // =========
 // Desejável
