1e79f4325749d70e29c99f2c06ae2df360f2d0e4
imagno <igor.magno@protonmail.com>
2022-01-24 20:34:16 -0300

    Iniciando desafio da semana 7

--- src/funcoes.js
+++ src/funcoes.js
@@ -3,6 +3,7 @@
 // Essencial
 // =========
 // Escreva uma função que receba um nome e retorne uma saudação para este nome: Tiago -> Olá, Tiago
+<<<<<<< HEAD
 const saudar = (nome) => `Olá, ${nome}`;
 // Escreva uma função que receba um nome completo e retorna apenas o primeiro nome: Tiago Lage Payne de Pádua -> Tiago
 const extrairPrimeiroNome = (nomeCompleto) => nomeCompleto.split(' ').shift();
@@ -13,12 +14,51 @@ const capitalizar = (palavra) => palavra.charAt(0).toUpperCase() + palavra.slice
 // (30, Alimentação) => 0
 // (10, Bebida) => 1
 const calculaImposto = (valor, categoria) => categoria !== 'Alimentação' ? valor * .1 : 0;
+=======
+function saudar(nome) {
+    return `Olá, ${nome}`
+}
+
+// Escreva uma função que receba um nome completo e retorna apenas o primeiro nome: Tiago Lage Payne de Pádua -> Tiago
+function extrairPrimeiroNome(nome) {
+    return `${nome.slice(0, 5)}`
+}
+
+// Escreva uma função que receba uma palavra e torna a primeira letra maiúscula e as outras minúsculas: tIaGo -> Tiago
+function capitalizar(nome) {
+    return `${nome[0].toUpperCase() + nome.slice(1).toLowerCase()}`
+}
+
+// Escreva uma função que recebe um preço original e uma categoria de produto e calcula o valor do imposto. Produtos da categoria Alimentação são isentos. Outros produtos tem um imposto de 10%.
+// (30, Alimentação) => 0
+// (10, Bebida) => 1
+function calculaImposto(valor, categoria) {
+    if (categoria === "Alimentação") {
+        return 0
+    } else {
+        return 1
+    }
+}
+
+>>>>>>> b478beee15138909a7a7a969b5b30644337c3523
 // Escreva uma função que recebe um preço original, uma categoria de produto e um cupom de desconto e calcula o preço com desconto. Se a categoria for Alimentação e o cupom for NULABSSA, deve ser feito um desconto de 50%. Caso contrário, não há nenhum desconto.
 // (30, Alimentação, NULABSSA) => 15
 // (10, Bebida, NULABSSA) => 10
 // (30, Alimentação, XPTO) => 30
 // (10, Bebida, XPTO) => 10
+<<<<<<< HEAD
 const calculaDesconto = (preco, categoria, cupom) => categoria === `Alimentação` && cupom === `NULABSSA` ? (preco / 2) : preco;
+=======
+function calculaDesconto(valor, categoria, cupom) {
+    if (categoria === "Alimentação" && cupom === "NULABSSA") {
+        return desconto = valor * 0.50
+
+    } else {
+        return valor
+    }
+}
+
+>>>>>>> b478beee15138909a7a7a969b5b30644337c3523
 // =========
 // Desejável
 // =========
@@ -26,11 +66,23 @@ const calculaDesconto = (preco, categoria, cupom) => categoria === `Alimentaçã
 // o valor default do comprimento máximo deve ser 5:
 // (teste, 10) -> teste
 // (fulano, 4) -> fula...
+<<<<<<< HEAD
 const truncar = (palavra, tamanho = 5) => palavra.length <= tamanho ? palavra : palavra.slice(0, tamanho).concat("...");
+=======
+function truncar(texto, comprimento = 5) {
+    if (texto.length > comprimento) {
+        return `${texto.slice(0, comprimento) + "..."}`
+    } else {
+        return `${texto.slice(0, 6)}`
+    }
+}
+
+>>>>>>> b478beee15138909a7a7a969b5b30644337c3523
 // Escreva uma função que valida se o texto informado está preenchido e retorna o texto sem espaços antes ou depois.
 // "" -> undefined
 // "   " -> undefined
 // "      Maria " -> "Maria"
+<<<<<<< HEAD
 const validaTextoPreenchido = (texto) => {
     if (!texto.trim()) {
         return undefined;
@@ -39,12 +91,23 @@ const validaTextoPreenchido = (texto) => {
         return texto.trim();
     }
 };
+=======
+function validaTextoPreenchido(texto) {
+    if (texto.trim() === "") {
+        return undefined
+    } else {
+        return texto.trim()
+    }
+}
+
+>>>>>>> b478beee15138909a7a7a969b5b30644337c3523
 // =======
 // Desafio
 // =======
 // Escreva uma função que valida se a string passada é uma data de nascimento válida, deve retornar um objeto Date sea data for válida ou NaN caso seja inválida.
 // 01/01/2000 -> Ok
 // 99/99/9999 -> NaN
+<<<<<<< HEAD
 const validaData = (date) => {
     const dataSeparada = date.split("/");
     const testDate = new Date(`${dataSeparada[2]}/${dataSeparada[1]}/${dataSeparada[0]}`);
@@ -56,3 +119,19 @@ const validaData = (date) => {
     }
 };
 module.exports = { saudar, extrairPrimeiroNome, capitalizar, calculaImposto, calculaDesconto, truncar, validaTextoPreenchido, validaData };
+=======
+function validaData(dataDeNascimento) {
+    let dia = dataDeNascimento.split("/")[0];
+    let mes = dataDeNascimento.split("/")[1];
+    let ano = dataDeNascimento.split("/")[2];
+    let dataCorreta = `${mes + "/" + dia + "/" + ano}`
+    if (Date.parse(dataCorreta)) {
+        return "Ok"
+    } else {
+        return Date.parse(dataCorreta)
+    }
+
+}
+
+module.exports = { saudar, extrairPrimeiroNome, capitalizar, calculaImposto, calculaDesconto, truncar, validaTextoPreenchido, validaData };
+>>>>>>> b478beee15138909a7a7a969b5b30644337c3523
