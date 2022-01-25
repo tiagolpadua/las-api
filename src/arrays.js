63f17582618e4509d2ad849237bc92d911544ed8
Glauber Santana <glauber.silva14@gmail.com>
2022-01-25 12:54:19 -0300

    added: solutions for essential functions

--- src/arrays.js
+++ src/arrays.js
@@ -5,51 +5,66 @@ const { capitalizar } = require('./funcoes');
 // =========
 // Essencial
 // =========
+const ehValido = (arr) => Array.isArray(arr) && arr.length !== 0;
 
-// Crie uma função que recebe uma lista de preços e devolve o menor preço
-// ([10, 7, 8, 25, 8, 9, 100, 99]) => 7
-function obterMenorPreco(lista) {
-}
-
-// Crie uma função que recebe uma lista de preços e devolve o maior preço
-// ([10, 7, 8, 25, 8, 9, 100, 99]) => 100
-function obterMaiorPreco(lista) {
-}
+const obterMenorPreco = (lista) => ehValido(lista) ? Math.min(...lista) : undefined;
+const obterMaiorPreco = (lista) => ehValido(lista) ? Math.max(...lista) : undefined;
 
 // Crie uma função que receba uma lista de nomes e devolve a lista de nomes capitalizados
 // (["tiago", "Alexandre", "kamillA"]) => ["Tiago", "Alexandre", "Kamilla"]
 function capitalizarNomes(nomes) {
+  if (!ehValido(nomes)) {
+      return undefined;
+  }
+  const a = [];
+  const b = [];
+  const c = [];
+  for (let i = 0; i < nomes.length; i++) {
+      a.push(nomes[i].toLowerCase());
+  }
+  for (let i = 0; i < nomes.length; i++) {
+      b.push(a[i].charAt(0).toUpperCase());
+  }
+  for (let i = 0; i < nomes.length; i++) {
+      c.push(`${b[i]}${a[i].slice(1, a[i].length)}`);
+  }
+  return c;
 }
 
-// Crie uma função que recebe o nome de uma categoria e devolve o desconto associado a esta categoria,
-// ou 0 se não houver desconto.
-// Utilize as listas que já estão na função para implementar seu código.
-// ('Alimentação') => 30
-// ('Infantil') => 15
-function obterDescontoCategoria(categoria) {
-    const categorias = ['Alimentação', 'Infantil'];
-    const descontos = [30, 15]
+const obterDescontoCategoria = (categoria) => {
+	const categorias = ['Alimentação', 'Infantil'];
+	const descontos = [30, 15];
+	if(!categorias.includes(categoria)){
+		return 0;c
+	}
+	return (descontos.at(categorias.indexOf(categoria)));
 }
 
-// Crie uma função que recebe uma lista de preços de produtos e um valor máximo de orçamento
-// e retorna uma lista com os preços menores ou iguais ao valor do orçamento informado
-// ([5, 7, 9, 50, 20], 9) => [5, 7, 9]
-function obterPrecosLimitadosAoOrcamento(lista, precoMaximo) {
+const obterPrecosLimitadosAoOrcamento = (lista,precoMaximo) => {
+	if(!ehValido(lista)){
+		return undefined;
+	}
+	return lista.filter((valor) => valor <= precoMaximo ? valor : false);
 }
 
-// Crie uma função que recebe uma lista de preços de produtos de uma compra
-// e retorna o valor total da compra
-// [10, 30, 5, 15] => 60
-function calcularTotalDaCompra(lista) {
+const calcularTotalDaCompra = (lista) => {
+  if(!ehValido(lista)){ 
+		return undefined;
+	}
+	return lista.reduce((anterior, atual) => anterior + atual);
 }
 
 // =========
 // Desejável
 // =========
-
-// Crie uma função que recebe uma lista de preços de produtos e retorna uma lista com o menor e o maior preço
-// ([10, 7, 8, 25, 8, 9, 100, 99]) => [7, 100]
-function obterMenorEMaiorPrecos(lista) {
+const obterMenorEMaiorPrecos = function (lista) {
+	if (!ehValido(lista)) {
+      return undefined;
+  }
+	const valoresMinMax = [];
+	valoresMinMax.push(lista.reduce((anterior, atual) => anterior < atual ? anterior : atual));
+	valoresMinMax.push(lista.reduce((anterior, atual) => anterior > atual ? anterior : atual));
+	return valoresMinMax;
 }
 
 // Crie uma função que recebe uma lista de preços de produtos, um valor inferior e um valor superior de orçamento.
@@ -57,6 +72,7 @@ function obterMenorEMaiorPrecos(lista) {
 // Valide se o orçamento está correto, ou seja, se o menor valor é igual ou inferior ao maior valor, caso contrário, retorne undefined.
 // ([10, 7, 8, 25, 8, 9, 100, 99], 9, 30) => [10, 25, 9]
 function obterPrecosDentroDoOrcamento(lista, menorValor, maiorValor) {
+
 }
 
 // Crie uma função que recebe uma categoria e um cupom e aplica um acréscimo de 10% no desconto da categoria, se o cupom for válido
