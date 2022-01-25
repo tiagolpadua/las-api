b018cf39d6bcfa4e2755bac79f17fdee96822e7e
Iago Teixeira <iago.teixeira@outlook.com.br>
2022-01-24 22:31:32 -0300

    Essenciais concluídos

--- src/arrays.js
+++ src/arrays.js
@@ -9,16 +9,47 @@ const { capitalizar } = require('./funcoes');
 // Crie uma função que recebe uma lista de preços e devolve o menor preço
 // ([10, 7, 8, 25, 8, 9, 100, 99]) => 7
 function obterMenorPreco(lista) {
+    let menorNumero = 99
+    let verificaTipo = typeof(lista)
+    if(verificaTipo == "string" || lista.length === 0){
+      return undefined
+    }
+    for (i = 0; i < lista.length; i++){
+        if(lista[i] < menorNumero){
+          menorNumero = lista[i]
+        }
+      }
+    return menorNumero
+   
 }
 
 // Crie uma função que recebe uma lista de preços e devolve o maior preço
 // ([10, 7, 8, 25, 8, 9, 100, 99]) => 100
 function obterMaiorPreco(lista) {
+    let maiorNumero = 0
+    if(typeof(lista) == "string" || lista.length === 0){
+      return undefined
+    }
+    for (i = 0; i < lista.length; i++){
+        if(lista[i] > maiorNumero){
+          maiorNumero = lista[i]
+        }
+      }
+    return maiorNumero
 }
 
 // Crie uma função que receba uma lista de nomes e devolve a lista de nomes capitalizados
 // (["tiago", "Alexandre", "kamillA"]) => ["Tiago", "Alexandre", "Kamilla"]
 function capitalizarNomes(nomes) {
+    const nomesCapitalizados = []
+    if(typeof(nomes) == "string" || nomes.length === 0){
+      return undefined
+    }
+    for (i = 0; i < nomes.length; i++){
+        nomesCapitalizados.push(`${nomes[i][0].toUpperCase()}${nomes[i].slice(1).toLowerCase()}`) 
+      }
+    return nomesCapitalizados
+
 }
 
 // Crie uma função que recebe o nome de uma categoria e devolve o desconto associado a esta categoria,
@@ -29,18 +60,36 @@ function capitalizarNomes(nomes) {
 function obterDescontoCategoria(categoria) {
     const categorias = ['Alimentação', 'Infantil'];
     const descontos = [30, 15]
+    const categoriasEdescontos = categorias.concat(descontos)
+    if (categoria == categoriasEdescontos[0]){
+      return categoriasEdescontos[2]     
+    }else if(categoria == categoriasEdescontos[1]){
+      return categoriasEdescontos[3]  
+    }else{
+      return 0
+    }
 }
 
 // Crie uma função que recebe uma lista de preços de produtos e um valor máximo de orçamento
 // e retorna uma lista com os preços menores ou iguais ao valor do orçamento informado
 // ([5, 7, 9, 50, 20], 9) => [5, 7, 9]
 function obterPrecosLimitadosAoOrcamento(lista, precoMaximo) {
+    if(lista.length === 0 || typeof(lista) == 'string'){
+        return undefined
+      }
+    let DentroDoOrcamento = lista.filter((item) => item <= precoMaximo)
+    return DentroDoOrcamento
 }
 
 // Crie uma função que recebe uma lista de preços de produtos de uma compra
 // e retorna o valor total da compra
 // [10, 30, 5, 15] => 60
 function calcularTotalDaCompra(lista) {
+    if(lista.length === 0 || typeof(lista) == 'string'){
+        return undefined
+      }
+    let valorFinal = lista.reduce((ac,total) => ac + total)
+    return valorFinal
 }
 
 // =========
