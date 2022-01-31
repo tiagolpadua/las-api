401f37173d99f64b1905b029de6947d0a795cae1
ricardo <rosastaricardo84@gmail.com>
2022-01-31 13:24:00 -0300

    Melhorando nomes variaveis cupom

--- src/arrays.js
+++ src/arrays.js
@@ -26,12 +26,12 @@ function capitalizarNomes(nomes) {
     const nomesCapitalizados = /^[A-Z][a-z]+$/g;
 
     if (nomes.length === 0 || !Array.isArray(nomes)) return undefined;
-    
-    return nomes.map(nome => { 
-        
-    if(!nome.match(nomesCapitalizados)) return capitalizar(nome);
-    
-    return nome;
+
+    return nomes.map(nome => {
+
+        if (!nome.match(nomesCapitalizados)) return capitalizar(nome);
+
+        return nome;
     });
 }
 
@@ -44,8 +44,8 @@ function obterDescontoCategoria(categoria) {
     const categorias = ['Alimentação', 'Infantil'];
     const descontos = [30, 15];
     const indexDesconto = categorias.indexOf(categoria);
-    
-    return indexDesconto !== -1 ?  descontos[indexDesconto] :  0;
+
+    return indexDesconto !== -1 ? descontos[indexDesconto] : 0;
 }
 
 // Crie uma função que recebe uma lista de preços de produtos e um valor máximo de orçamento
@@ -54,7 +54,7 @@ function obterDescontoCategoria(categoria) {
 function obterPrecosLimitadosAoOrcamento(lista, precoMaximo) {
 
     if (lista.length === 0 || !Array.isArray(lista)) return undefined;
-    
+
     return lista.filter(precos => precos <= precoMaximo && precos > 0);
 }
 
@@ -64,7 +64,7 @@ function obterPrecosLimitadosAoOrcamento(lista, precoMaximo) {
 function calcularTotalDaCompra(lista) {
 
     if (lista.length === 0 || !Array.isArray(lista)) return undefined;
-    return lista.reduce((anterior , atual) => anterior + atual);
+    return lista.reduce((anterior, atual) => anterior + atual);
 
 }
 
@@ -75,9 +75,9 @@ function calcularTotalDaCompra(lista) {
 // Crie uma função que recebe uma lista de preços de produtos e retorna uma lista com o menor e o maior preço
 // ([10, 7, 8, 25, 8, 9, 100, 99]) => [7, 100]
 function obterMenorEMaiorPrecos(lista) {
-    
+
     if (lista.length === 0 || !Array.isArray(lista)) return undefined;
-    return lista.sort((precoA, precoB) => precoA - precoB).filter((item , index) => index === 0 || index === lista.length-1);
+    return lista.sort((precoA, precoB) => precoA - precoB).filter((item, index) => index === 0 || index === lista.length - 1);
 }
 
 // Crie uma função que recebe uma lista de preços de produtos, um valor inferior e um valor superior de orçamento.
@@ -85,11 +85,11 @@ function obterMenorEMaiorPrecos(lista) {
 // Valide se o orçamento está correto, ou seja, se o menor valor é igual ou inferior ao maior valor, caso contrário, retorne undefined.
 // ([10, 7, 8, 25, 8, 9, 100, 99], 9, 30) => [10, 25, 9]
 function obterPrecosDentroDoOrcamento(lista, menorValor, maiorValor) {
-    
+
     if (lista.length === 0 || !Array.isArray(lista)) return undefined;
 
-    if(menorValor >= maiorValor) return undefined;
-    
+    if (menorValor >= maiorValor) return undefined;
+
     return lista.filter(preco => preco >= menorValor && preco <= maiorValor);
 }
 
@@ -104,7 +104,7 @@ function obterPrecosDentroDoOrcamento(lista, menorValor, maiorValor) {
 // Utilize a função descontoCategoria criada anteriormente
 function obterDescontoTotal(categoria, cupom) {
 
-   return (/(CUPOM-)?INVALIDO/g).test(cupom) ? obterDescontoCategoria(categoria) : obterDescontoCategoria(categoria) + 10;
+    return (/(CUPOM-)?INVALIDO/g).test(cupom) ? obterDescontoCategoria(categoria) : obterDescontoCategoria(categoria) + 10;
 }
 
 // Crie uma função que recebe uma lista de preços e uma lista de categorias de produtos e
@@ -116,14 +116,14 @@ function calcularTotalDaCompraComDescontos(precos, categorias, cupom) {
     const verificar = Array.from(arguments);
     let totalCompraDesconto = 0;
 
-    for(let i = 0 ; i < verificar.length-1; i++){
+    for (let i = 0; i < verificar.length - 1; i++) {
 
-      if(verificar[i].length === 0 || !Array.isArray(verificar[i])) return undefined;
+        if (verificar[i].length === 0 || !Array.isArray(verificar[i])) return undefined;
     }
 
-    for(let i = 0; i < precos.length; i++){
-        
-         totalCompraDesconto += (precos[i] * ( 1 - (obterDescontoTotal(categorias[i], cupom)/100)) );
+    for (let i = 0; i < precos.length; i++) {
+
+        totalCompraDesconto += (precos[i] * (1 - (obterDescontoTotal(categorias[i], cupom) / 100)));
     }
 
     return totalCompraDesconto;
@@ -136,11 +136,11 @@ function capitalizarNomeCompleto(nomeCompleto) {
 
     let nomeCaptalizado = nomeCompleto.split(" ").map(nome => {
 
-       return nome.length > 3 ? nome[0].toUpperCase() + nome.substr(1 , nome.length-1).toLowerCase() : nome
-     
-     });
+        return nome.length > 3 ? capitalizar(nome) : nome ;
 
-     return nomeCaptalizado.join(" ");
+    });
+
+    return nomeCaptalizado.join(" ");
 }
 
 // =======
@@ -158,96 +158,96 @@ function capitalizarNomeCompleto(nomeCompleto) {
 function gerarCupomFiscal(listaNomesProdutos, listaPrecosProdutos, listaCategoriasProdutos, cupom) {
 
     const verificar = Array.from(arguments);
-    
-    for(let i = 0 ; i < verificar.length-1; i++){
 
-      if(verificar[i].length === 0 || !Array.isArray(verificar[i])) return undefined;
+    for (let i = 0; i < verificar.length - 1; i++) {
+
+        if (verificar[i].length === 0 || !Array.isArray(verificar[i])) return undefined;
     }
-  
+
     // variaveis da nota
     let nome = listaNomesProdutos;
-    let v = listaPrecosProdutos;
-    let d1 = v[0] * (obterDescontoTotal(listaCategoriasProdutos[0], cupom)/100);
-    let d2 = v[1] * (obterDescontoTotal(listaCategoriasProdutos[1], cupom)/100);
+    let valorProduto = listaPrecosProdutos;
+    let desconto1 = valorProduto[0] * (obterDescontoTotal(listaCategoriasProdutos[0], cupom) / 100);
+    let desconto2 = valorProduto[1] * (obterDescontoTotal(listaCategoriasProdutos[1], cupom) / 100);
     let imposto = 15;
-    let tot1 = (listaPrecosProdutos[0] * 1.15) - d1;
-    let tot2 =  (listaPrecosProdutos[1]) - d2;
-    let subT = tot1 + tot2;
-    let cup = 3;
-    let totF = subT - cup;
+    let total1 = (listaPrecosProdutos[0] * 1.15) - desconto1;
+    let total2 = (listaPrecosProdutos[1]) - desconto2;
+    let subTotal = total1 + total2;
+    let cupomRodape = 3;
+    let totalFinal = subTotal - cupomRodape;
 
     // variaveis montar cupom
-    let title = ['Nome', 'Valor' , 'Desconto', 'Imposto', 'Total' ];
-  
-    function criarHeader(titulo){
-    
-    const [nome , valor, desconto, imposto, total] = titulo ;
-    let space = ["" , "" , "" , "" , ""];
-    
-        if(nome.length < 15) space[0] = " ".repeat(15 - nome.length );
-        if(valor.length < 10) space[1] = " ".repeat(10 - valor.length );
-        if(desconto.length < 10) space[2] = " ".repeat(10 - desconto.length );
-        if(imposto.length < 8) space[3] = " ".repeat(8 - imposto.length );
-        if(total.length < 8) space[4] = " ".repeat(9 - total.length );
-    
-    let header =`${nome}${space[0]}${valor}${space[1]}${desconto}${space[2]}${imposto}${space[3]}${total}     `;
-    
+    let title = ['Nome', 'Valor', 'Desconto', 'Imposto', 'Total'];
+
+    function criarHeader(titulo) {
+
+        const [nome, valor, desconto, imposto, total] = titulo;
+        let space = ["", "", "", "", ""];
+
+        if (nome.length < 15) space[0] = " ".repeat(15 - nome.length);
+        if (valor.length < 10) space[1] = " ".repeat(10 - valor.length);
+        if (desconto.length < 10) space[2] = " ".repeat(10 - desconto.length);
+        if (imposto.length < 8) space[3] = " ".repeat(8 - imposto.length);
+        if (total.length < 8) space[4] = " ".repeat(9 - total.length);
+
+        let header = `${nome}${space[0]}${valor}${space[1]}${desconto}${space[2]}${imposto}${space[3]}${total}     `;
+
         return header;
     }
 
-    function criarLinhaProduto(nome , valor , desconto , imposto , total){
-        
+    function criarLinhaProduto(nome, valor, desconto, imposto, total) {
+
         let novoValor = valor.toFixed(2).replace(".", ",");
         let novoDesconto = desconto.toFixed(2).replace(".", ",");
-        let novoImposto = imposto+"%";
+        let novoImposto = imposto + "%";
         let notoTotal = total.toFixed(2).replace(".", ",");
-        let space = ["" , "" , "" , "" , ""];
-    
-        if(!imposto) novoImposto = "";
-        
-        if(nome.length < 15) space[0] = " ".repeat(15 - nome.length );
-        if(novoValor.length < 9) space[1] = " ".repeat(9 - (novoValor.length + 2) );
-        if(novoDesconto.length < 9) space[2] = " ".repeat(9 - (novoDesconto.length + 2) );
-        if(novoImposto.length < 7) space[3] = " ".repeat(7 - (novoImposto.length) );
+        let space = ["", "", "", "", ""];
+
+        if (!imposto) novoImposto = "";
+
+        if (nome.length < 15) space[0] = " ".repeat(15 - nome.length);
+        if (novoValor.length < 9) space[1] = " ".repeat(9 - (novoValor.length + 2));
+        if (novoDesconto.length < 9) space[2] = " ".repeat(9 - (novoDesconto.length + 2));
+        if (novoImposto.length < 7) space[3] = " ".repeat(7 - (novoImposto.length));
         else " ".repeat(7);
-        if(notoTotal.length < 9) space[4] = " ".repeat(9 - (notoTotal.length + 2) );
-       
-        
-        let linha =`${nome}${space[0]}R$${space[1]}${novoValor} R$${space[2]}${novoDesconto} ${space[3]}${novoImposto} R$${space[4]}${notoTotal} `;
-        
+        if (notoTotal.length < 9) space[4] = " ".repeat(9 - (notoTotal.length + 2));
+
+
+        let linha = `${nome}${space[0]}R$${space[1]}${novoValor} R$${space[2]}${novoDesconto} ${space[3]}${novoImposto} R$${space[4]}${notoTotal} `;
+
         return linha;
     }
 
-    function criarRodape(nome , valor){
-    
-    let novoValor = valor.toFixed(2).replace(".", ",");
-  
-    let space = ["" , "" , "" , "" , ""];
+    function criarRodape(nome, valor) {
 
-        if(nome.length < 43) space[0] = " ".repeat(43 - nome.length );
-        if(novoValor.length < 9) space[1] = " ".repeat(9 - (novoValor.length + 2) );
-        
-    let header =`${nome}${space[0]}R$${space[1]}${novoValor} `;
-    
-        return header;
+        let novoValor = valor.toFixed(2).replace(".", ",");
+
+        let space = ["", "", "", "", ""];
+
+        if (nome.length < 43) space[0] = " ".repeat(43 - nome.length);
+        if (novoValor.length < 9) space[1] = " ".repeat(9 - (novoValor.length + 2));
+
+        let rodape = `${nome}${space[0]}R$${space[1]}${novoValor} `;
+
+        return rodape;
     }
 
-    function criarFinal(nome , valor){
-    
+    function criarFinal(nome, valor) {
+
         let novoValor = valor.toFixed(2).replace(".", ",");
-      
-        let space = ["" , "" , "" , "" , ""];
-    
-            if(nome.length < 43) space[0] = " ".repeat(43 - nome.length );
-            if(novoValor.length < 9) space[1] = " ".repeat(9 - (novoValor.length + 2) );
-            
-        let header =`${nome}${space[0]}R$${space[1]}${novoValor}`;
-        
-            return header;
-        }
-    
-   let cupomFiscal = criarHeader(title)+ '\n' + criarLinhaProduto(nome[0] , v[0] , d1 , imposto , tot1)+ '\n' + criarLinhaProduto(nome[1] , v[1] , d2, '' , tot2)+ '\n' + criarRodape('Subtotal' , subT)+ '\n' + criarRodape('Cupom de Desconto: '+ cupom, cup)+ '\n' + criarFinal('Total', totF);
-   
+
+        let space = ["", "", "", "", ""];
+
+        if (nome.length < 43) space[0] = " ".repeat(43 - nome.length);
+        if (novoValor.length < 9) space[1] = " ".repeat(9 - (novoValor.length + 2));
+
+        let final = `${nome}${space[0]}R$${space[1]}${novoValor}`;
+
+        return final;
+    }
+
+    let cupomFiscal = criarHeader(title) + '\n' + criarLinhaProduto(nome[0], valorProduto[0], desconto1, imposto, total1) + '\n' + criarLinhaProduto(nome[1], valorProduto[1], desconto2, '', total2) + '\n' + criarRodape('Subtotal', subTotal) + '\n' + criarRodape('Cupom de Desconto: ' + cupom, cupomRodape) + '\n' + criarFinal('Total', totalFinal);
+
 
     return cupomFiscal
 }
