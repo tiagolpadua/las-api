bf31e53e54aa3b8eae03c1f0fecb56799329efbe
Ingrid-cruz <ingridsantos8767@gmail.com>
2022-02-03 17:53:11 -0300

    teste essencial

--- src/arrays.js
+++ src/arrays.js
@@ -11,21 +11,21 @@ const { capitalizar } = require('./funcoes');
 function naoEhLista(lista){
     return (!Array.isArray(lista) || lista.length === 0)
 }
-
 function obterMenorPreco(lista) {
     if (naoEhLista(lista)){ 
-       return undefined
-   } 
-    let menorPreco = lista[0]
-    for (let i = 1; i < lista.length; i++) {
-        if (lista[i] < menorPreco) {
-            menorPreco = lista[i]
-        }
-    } return menorPreco
+        return undefined
+    } 
+     let menorPreco = lista[0]
+     for (let i = 1; i < lista.length; i++) {
+         if (lista[i] < menorPreco) {
+             menorPreco = lista[i]
+         }
+     } return menorPreco
 }
+
 // Crie uma função que recebe uma lista de preços e devolve o maior preço
 // ([10, 7, 8, 25, 8, 9, 100, 99]) => 100
-function obterMaiorPreco(lista){
+function obterMaiorPreco(lista) {
     let maiorPreco = lista[0]
 
     if (naoEhLista(lista)){
@@ -37,14 +37,15 @@ function obterMaiorPreco(lista){
          }
     } return maiorPreco
 }
+
 // Crie uma função que receba uma lista de nomes e devolve a lista de nomes capitalizados
 // (["tiago", "Alexandre", "kamillA"]) => ["Tiago", "Alexandre", "Kamilla"]
 function capitalizarNomes(nomes) {
-   if (naoEhLista(nomes)){
-       return undefined
-   }
-    const nomesCapitalizados = nomes.map(nome => nome.charAt(0).toUpperCase() + nome.slice(1).toLowerCase())
-    return nomesCapitalizados
+    if (naoEhLista(nomes)){
+        return undefined
+    }
+     const nomesCapitalizados = nomes.map(nome => nome.charAt(0).toUpperCase() + nome.slice(1).toLowerCase())
+     return nomesCapitalizados
 }
 
 // Crie uma função que recebe o nome de uma categoria e devolve o desconto associado a esta categoria,
@@ -60,11 +61,12 @@ function obterDescontoCategoria(categoria) {
     } else if (categoria === categorias[1]){
         return descontos[1]
     } return 0
-    }
+}
+
 // Crie uma função que recebe uma lista de preços de produtos e um valor máximo de orçamento
 // e retorna uma lista com os preços menores ou iguais ao valor do orçamento informado
 // ([5, 7, 9, 50, 20], 9) => [5, 7, 9]
-function obterPrecosLimitadosAoOrcamento(lista, precoMaximo) {  
+function obterPrecosLimitadosAoOrcamento(lista, precoMaximo) {
     if (naoEhLista(lista)){ 
         return undefined
     } 
@@ -75,10 +77,11 @@ function obterPrecosLimitadosAoOrcamento(lista, precoMaximo) {
         }
     }  return precoLimite
 }
+
 // Crie uma função que recebe uma lista de preços de produtos de uma compra
 // e retorna o valor total da compra
 // [10, 30, 5, 15] => 60
-function calcularTotalDaCompra(lista){
+function calcularTotalDaCompra(lista) {
     if (naoEhLista(lista)){ 
         return undefined
     } 
@@ -87,9 +90,7 @@ function calcularTotalDaCompra(lista){
         soma += lista[i];
     }
         return soma
-} 
-
-
+}
 // =========
 // Desejável
 // =========
@@ -158,5 +159,5 @@ module.exports = {
     obterDescontoTotal,
     calcularTotalDaCompraComDescontos,
     capitalizarNomeCompleto,
-    gerarCupomFiscal,
-}
+    gerarCupomFiscal
+};
