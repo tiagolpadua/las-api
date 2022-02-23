e1afa43fb66fd57b63bd5931ee8120a2276ba77e
Ge Vasconcelos <geminvasconceelos@gmail.com>
2022-02-23 19:49:44 -0300

    listaCategoria e tentando a listaCuponsValido

--- src/api-service.js
+++ src/api-service.js
@@ -1,17 +1,48 @@
 const fetch = require("node-fetch");
 
 async function listaProdutos() {
-
-  fetch ("https://stupefied-keller-a2c79e.netlify.app/produtos.json")
+  await fetch ("https://stupefied-keller-a2c79e.netlify.app/produtos.json")
   .then(response => response.json()) // retorna uma promise
   .then(result => {
-    console.table(result);
-    result.forEach(element => {
+  //  onsole.table(result);
+     result.forEach(element => {
       console.table(element.nome);
       // dataProdutos = element.nome;
     });
-  
   });
 }
 
+async function listaCategoria() {
+  await fetch ("https://stupefied-keller-a2c79e.netlify.app/categorias.json")
+  .then(resCategoria => resCategoria.json()) // retorna uma promise
+  .then(resultCategoria => {
+    // console.table(resultCategoria);
+     resultCategoria.forEach(element => {
+      console.table(element.nome);
+      // dataProdutos = element.nome;
+    });
+  });
+}
+
+async function listaCuponsValidos() {
+  await fetch ("https://stupefied-keller-a2c79e.netlify.app/cupons.json")
+  .then(resCuponsValidos => resCuponsValidos.json()) // retorna uma promise
+  .then(resultCuponsValidos => {
+    
+    //  console.log(resultCuponsValidos);
+     resultCuponsValidos.forEach(element => {
+      if(element === "NULABSSA" || element === "ALURANU"){
+        console.table(element);
+      // dataProdutos = element.nome;
+       }
+    });
+    
+  });
+}
+
+
 console.log(listaProdutos());
+
+console.log(listaCategoria());
+
+console.log(listaCuponsValidos());
