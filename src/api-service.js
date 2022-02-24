71a7be680e8373af16d2266c0b66602e9b88976e
Tiago Lage Payne de Pádua <tiagolpadua@gmail.com>
2022-02-24 19:32:35 -0300

    live

---
+++ src/api-service.js
@@ -0,0 +1,25 @@
+const fetch = require("node-fetch");
+
+async function listarProdutos() {
+    const response = await fetch("https://stupefied-keller-a2c79e.netlify.app/produtos.json");
+    verificarErros(response);
+    const data = await response.json();
+    return data;
+}
+
+function verificarErros(response) {
+    if (response.status !== 200) {
+        throw new Error(`${response.statusText}: ${response.status}`);
+    }
+}
+
+async function listarCategorias() {
+    const response = await fetch("https://stupefied-keller-a2c79e.netlify.app/categorias.json");
+    verificarErros(response);
+    const data = await response.json();
+    return data;
+}
+
+module.exports = {
+    listarProdutos, listarCategorias
+};
