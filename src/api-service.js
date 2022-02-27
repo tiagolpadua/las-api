b9b7ea626ee8df0e527e8ca2240954f65f40033a
Ge Vasconcelos <geminvasconceelos@gmail.com>
2022-02-27 08:59:35 -0300

    api-service

--- src/api-service.js
+++ src/api-service.js
@@ -1,48 +1,39 @@
 const fetch = require("node-fetch");
 
-async function listaProdutos() {
-  await fetch ("https://stupefied-keller-a2c79e.netlify.app/produtos.json")
-  .then(response => response.json()) // retorna uma promise
-  .then(result => {
-  //  onsole.table(result);
-     result.forEach(element => {
-      console.table(element.nome);
-      // dataProdutos = element.nome;
-    });
-  });
+async function listarProdutos() {
+  let result = await fetch("https://stupefied-keller-a2c79e.netlify.app/produtos.json");
+  let data = result.json();
+  return data;
 }
 
-async function listaCategoria() {
-  await fetch ("https://stupefied-keller-a2c79e.netlify.app/categorias.json")
-  .then(resCategoria => resCategoria.json()) // retorna uma promise
-  .then(resultCategoria => {
-    // console.table(resultCategoria);
-     resultCategoria.forEach(element => {
-      console.table(element.nome);
-      // dataProdutos = element.nome;
-    });
-  });
+async function listarCategoria() {
+  let result = await fetch("https://stupefied-keller-a2c79e.netlify.app/categorias.json");
+  let data = result.json();
+  return data;
 }
 
-async function listaCuponsValidos() {
-  await fetch ("https://stupefied-keller-a2c79e.netlify.app/cupons.json")
-  .then(resCuponsValidos => resCuponsValidos.json()) // retorna uma promise
-  .then(resultCuponsValidos => {
+async function listarCuponsValidos() {
+  // await fetch ("https://stupefied-keller-a2c79e.netlify.app/cupons.json")
+  // .then(resCuponsValidos => resCuponsValidos.json()) // retorna uma promise
+  // .then(resultCuponsValidos => {
     
-    //  console.log(resultCuponsValidos);
-     resultCuponsValidos.forEach(element => {
-      if(element === "NULABSSA" || element === "ALURANU"){
-        console.table(element);
-      // dataProdutos = element.nome;
-       }
-    });
+  //   //  console.log(resultCuponsValidos);
+  //    resultCuponsValidos.forEach(element => {
+  //     if(element === "NULABSSA" || element === "ALURANU"){
+  //       console.table(element);
+  //     // dataProdutos = element.nome;
+  //      }
+  //   });
     
-  });
+  // });
+  let result = await fetch("https://stupefied-keller-a2c79e.netlify.app/cupons.json");
+  let data = result.json();
+  return data;
+  
 }
 
-
-console.log(listaProdutos());
-
-console.log(listaCategoria());
-
-console.log(listaCuponsValidos());
+module.exports = {
+  listarProdutos,
+  listarCategoria, 
+  listarCuponsValidos
+};
--- src/api-service.test.js
+++ src/api-service.test.js
@@ -3,13 +3,15 @@
 // - https://stupefied-keller-a2c79e.netlify.app/categorias.json
 // - https://stupefied-keller-a2c79e.netlify.app/cupons.json
 
+const { listarProdutos } = require("./api-service");
+
 // Utilize as respostas "Mocadas" disponíveis em ../mocks
 
 // Utilize a função de mock do Jest para mocar as respostas no node-fetch: https://jestjs.io/pt-BR/docs/mock-functions
 
 // Utilize a lib node-fetch em sua versão 2 - https://www.npmjs.com/package/node-fetch
 
-// const PRODUTOS_MOCK = require("../mocks/produtos.json");
+const PRODUTOS_MOCK = require("../mocks/produtos.json");
 // const CATEGORIAS_MOCK = require("../mocks/categorias.json");
 // const CUPONS_MOCK = require("../mocks/cupons.json");
 
@@ -17,6 +19,11 @@ describe("Essencial", () => {
   // Crie uma função e o teste desta função, que lista os produtos a partir da API e retorna um JSON
   // com esta lista de produtos
   // test: "Deve ter uma função que lista os produtos."
+  test("Deve ter uma função que lista os produtos.", async () => {
+    const produtos = await listarProdutos();
+     expect(produtos).toEqual (PRODUTOS_MOCK);
+  });
+
 
   // Crie uma função e o teste desta função, que lista as categorias a partir da API e retorna um JSON
   // com esta lista de categorias
@@ -30,7 +37,7 @@ describe("Essencial", () => {
   // ela deve lançar uma Exceção com o seguinte formato: `${response.statusText}: ${response.status}`
   // test "Deve tratar erros 404."
 
-  test("Uma tautologia.", () => {
-    expect(1 === 1).toBe(true);
-  });
+  // test("Uma tautologia.", () => {
+  //   expect(1 === 1).toBe(true);
+  // });
 });
