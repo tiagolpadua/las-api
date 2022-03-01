14c2b1a4e5bfb6f11ed13d6eff26c1e898df5b4c
Emily Menezes <84086374+emilylmenezes@users.noreply.github.com>
2022-02-28 22:28:48 -0300

    Finalizando os Desejáveis

--- src/api-service.test.js
+++ src/api-service.test.js
@@ -20,59 +20,66 @@ describe("Essencial", () => {
   // Crie uma função e o teste desta função, que lista os produtos a partir da API e retorna um JSON
   // com esta lista de produtos
   // test: "Deve ter uma função que lista os produtos."
+  
+  test("Deve testar uma função que lista os produtos", async () => {
+    fetch.mockResolvedValue({
+      status: 200,
+      json: () => Promise.resolve(PRODUTOS_MOCK),
+    });
+    const produtos = await listarProdutos();
+    expect(produtos).toEqual(PRODUTOS_MOCK);
+  });
 
   // Crie uma função e o teste desta função, que lista as categorias a partir da API e retorna um JSON
   // com esta lista de categorias
   // test: "Deve ter uma função que lista as categorias."
 
+  test("Deve testar uma função que lista as categorias", async () => {
+    fetch.mockResolvedValue({
+     status: 200,
+     json: () => Promise.resolve(CATEGORIAS_MOCK),
+   });
+   const categorias = await listarCategoria();
+   expect(categorias).toEqual(CATEGORIAS_MOCK);
+   });
+
   // Crie uma função e o teste desta função, que lista os cupons válidos a partir da API e retorna um JSON
   // com esta lista de cupons válidos
   // test: "Deve ter uma função que lista os cupons válidos."
 
+  test("Deve testar uma função que lista os cupons", async () => {
+    fetch.mockResolvedValue({
+      status: 200,
+      json: () => Promise.resolve(CUPONS_MOCK),
+    });
+    const cupom = await listarCupom();
+    expect(cupom).toEqual(CUPONS_MOCK);
+  });
+  });
+
   // Crie um teste para quando qualquer API for acionada, caso o status code seja diferente de 200,
   // ela deve lançar uma Exceção com o seguinte formato: `${response.statusText}: ${response.status}`
   // test "Deve tratar erros 404."
+
+  test("Deve tratar erros 404.", async () => {
+    fetch.mockResolvedValue({
+      status: 404,
+      statusText: "Not Found"
+    });
+  
+    await expect(listarProdutos()).rejects.toThrow("Not Found: 404");
+    await expect(listarCategoria()).rejects.toThrow("Not Found: 404");
+    await expect(listarCupom()).rejects.toThrow("Not Found: 404");
+  });
 
 //   test("Uma tautologia.", () => {
 //     expect(1 === 1).toBe(true);
 //   });
 
 
- test("Deve testar uma função que lista os produtos", async () => {
-  fetch.mockResolvedValue({
-    status: 200,
-    json: () => Promise.resolve(PRODUTOS_MOCK),
-  });
-  const produtos = await listarProdutos();
-  expect(produtos).toEqual(PRODUTOS_MOCK);
-});
 
-test("Deve testar uma função que lista as categorias", async () => {
- fetch.mockResolvedValue({
-  status: 200,
-  json: () => Promise.resolve(CATEGORIAS_MOCK),
-});
-const categorias = await listarCategoria();
-expect(categorias).toEqual(CATEGORIAS_MOCK);
-});
 
-test("Deve testar uma função que lista os cupons", async () => {
-  fetch.mockResolvedValue({
-    status: 200,
-    json: () => Promise.resolve(CUPONS_MOCK),
-  });
-  const cupom = await listarCupom();
-  expect(cupom).toEqual(CUPONS_MOCK);
-});
-});
 
-test("Deve tratar erros 404.", async () => {
-  fetch.mockResolvedValue({
-    status: 404,
-    statusText: "Not Found"
-  });
 
-  await expect(listarProdutos()).rejects.toThrow("Not Found: 404");
-  await expect(listarCategoria()).rejects.toThrow("Not Found: 404");
-  await expect(listarCupom()).rejects.toThrow("Not Found: 404");
-});
+
+
--- src/cli.js
+++ src/cli.js
@@ -1,17 +1,46 @@
-async function processarOpcao(opcao) {
+const { listarProdutos, listarCategoria} = require("./api-service");
+const {incluirPrecoFormatado, obterDescontoCategoria} = require("./objetos");
+
+
+async function processarOpcao(opcao){
+  const listandoOsProdutos = await listarProdutos();
+  const listandoAsCategorias = await listarCategoria();
   // TODO
-  console.log(opcao);
-}
+  switch(opcao){
+    //console.log(opcao);
+    case "produtos": return listandoOsProdutos;
 
-async function run() {
-  const opcao = process.argv[2];
-  const saida = await processarOpcao(opcao);
-  console.table(saida);
-}
+    case "produtos-formatados": 
+    return listandoOsProdutos.map((i) => {
+    let formatarValor = incluirPrecoFormatado(i);
+    formatarValor["preco"] = formatarValor["precoFormatado"];
+    delete formatarValor.precoFormatado;
+    return formatarValor;
+  });
 
-if (require.main === module) {
-  run();
-}
+  case "categorias": return listandoAsCategorias;
+
+  case "descontos": return listandoOsProdutos.map((i) => {
+    let formatarValor = incluirPrecoFormatado(i);
+    formatarValor["preco"] = formatarValor["precoFormatado"];
+    formatarValor["desconto"] = obterDescontoCategoria(formatarValor["categoria"]);
+    delete formatarValor.precoFormatado;
+    return formatarValor;
+  });
+  case undefined: throw new Error("Informe uma opção.");
+  default: throw new Error(`Opção inválida: ${opcao}`);
+} 
+ }
+
+// async function run() {
+//   const opcao = process.argv[2];
+//   const saida = await processarOpcao(opcao);
+//   console.table(saida);
+// }
+
+// if (require.main === module) {
+//   run();
+// }
 
 module.exports = {
   processarOpcao,
