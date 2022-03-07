2df92ab11985d112a53b1059269aa0381c1c4f93
Glauber S Santana <glauber.silva14@gmail.com>
2022-03-07 19:37:06 -0300

    fix: file had a missing bracket

--- src/objetos.test.js
+++ src/objetos.test.js
@@ -199,10 +199,11 @@ describe("Desejável", () => {
 
     expect(obterDescontoTotal("Infantil", cupomAluranu)).toEqual(30);
 
-    test("Deve calcular o total da compra com descontos.", () => {
-        expect(calcularTotalDaCompraComDescontos([], 1, "foo")).toBeUndefined();
+    expect(obterDescontoTotal("Bebida", cupomAluranu)).toEqual(15);
 
-        expect(calcularTotalDaCompraComDescontos("foo", [], "foo")).toBeUndefined();
+    expect(
+      obterDescontoTotal("Alimentação", { texto: "ALURANU", desconto: -99 })
+    ).toEqual(30);
 
     expect(
       obterDescontoTotal("Bebida", { texto: "CUPOM-INVALIDO", desconto: 15 })
