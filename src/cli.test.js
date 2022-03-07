a8d7981e906b3f454142abb71c0d3949edb4e157
Tiago Lage Payne de Pádua <tiagolpadua@gmail.com>
2022-03-07 19:30:08 -0300

    live

--- src/cli.test.js
+++ src/cli.test.js
@@ -1,16 +1,26 @@
+const { processarOpcao } = require("./cli");
+const { listarProdutos } = require("./api-service");
+
 // Utilize as respostas "Mocadas" disponíveis em ../mocks
 // Utilize a função de mock do Jest para mocar as respostas no api-service: https://jestjs.io/pt-BR/docs/mock-functions
 
-// const PRODUTOS_MOCK = require("../mocks/produtos.json");
+const PRODUTOS_MOCK = require("../mocks/produtos.json");
 // const PRODUTOS_FORMATADO_MOCK = require("../mocks/produtos-formatado.json");
 // const CATEGORIAS_MOCK = require("../mocks/categorias.json");
 // const PRODUTOS_DESCONTO_MOCK = require("../mocks/produtos-desconto.json");
+
+jest.mock("./api-service");
 
 describe("Desejável", () => {
   // Crie uma opção e o teste desta opção, que lista os produtos utilizando
   // o api-service quando é informado argumento 'produtos' na linha de comandos.
   // Utilize PRODUTOS_MOCK
   // test "Deve listar os produtos."
+  test("Deve listar os produtos.", async () => {
+    listarProdutos.mockResolvedValue(PRODUTOS_MOCK);
+    const produtos = await processarOpcao("produtos");
+    expect(produtos).toEqual(PRODUTOS_MOCK);
+  });
 
   // Crie uma opção e o teste desta opção, que lista os produtos com o preço formatado utilizando
   // o api-service quando é informado argumento 'produtos-formatados' na linha de comandos.
@@ -30,12 +40,13 @@ describe("Desejável", () => {
   // Valide se a opção informada é válida (não esqueça do teste :-)), se não for,
   // emita uma exceção: "Opção inválida: ${opcao-informada}"
   // test "Deve emitir erro se informar uma opção inválida."
+  test("Deve emitir erro se informar uma opção inválida.", async () => {
+    await expect(processarOpcao("uma-opcao-invalida")).rejects.toThrow(
+      "Opção inválida: uma-opcao-invalida"
+    );
+  });
 
   // Valide se foi informada alguma opção (não esqueça do teste :-)), se não for,
   // emita uma exceção: "Informe uma opção."
   // test "Deve emitir erro não informar uma opção."
-
-  test("Uma tautologia.", () => {
-    expect(1 === 1).toBe(true);
-  });
 });
