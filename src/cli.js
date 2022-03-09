7340b08a0f96c6af3cac4961aef8456c99e0e309
João Marcelo <joao.marcelo.c17@gmail.com>
2022-03-09 12:25:12 -0300

    trabalhando no Desejavel

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,32 @@
+const {listarProdutos, listarCategorias}= require("./api-service");
+
+
 async function processarOpcao(opcao) {
-  // TODO
   console.log(opcao);
+  if(opcao===undefined){
+    throw new Error("Informe uma opção.");
+  }
+  let retorno;
+  let produtos;
+  let categorias;
+  switch (opcao.toUpperCase()) {
+    case "PRODUTOS":
+      retorno = await listarProdutos();
+      break;
+    case "DESCONTOS":
+      produtos = await listarProdutos();
+      categorias = await listarCategorias();
+
+      retorno = produtos.map((p)=>{
+        const categoria = categorias.find((c)=>p.categoria === c.nome);
+        return{...p, desconto: categoria?.desconto || 0};
+      } );
+
+      break;
+    default:
+      throw new Error(`Opção invalida: ${opcao}`);
+  }
+  return retorno;
 }
 
 async function run() {
