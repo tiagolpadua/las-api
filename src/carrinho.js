4a920a1991453fb533706692d1025a946b9413f2
MarckoAj <96995466+MarckoAj@users.noreply.github.com>
2022-03-12 12:37:01 -0300

    essesncial concluido

--- src/carrinho.js
+++ src/carrinho.js
@@ -1,18 +1,18 @@
-// const readline = require("readline");
+const readline = require("readline");
 
-// function askQuestion(query) {
-//   const rl = readline.createInterface({
-//     input: process.stdin,
-//     output: process.stdout,
-//   });
+function askQuestion(query) {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
 
-//   return new Promise((resolve) =>
-//     rl.question(query, (ans) => {
-//       rl.close();
-//       resolve(ans);
-//     })
-//   );
-// }
+  return new Promise((resolve) =>
+    rl.question(query, (ans) => {
+      rl.close();
+      resolve(ans);
+    })
+  );
+}
 
 async function run() {
   // TODO
@@ -21,3 +21,4 @@ async function run() {
 if (require.main === module) {
   run();
 }
+console.log(askQuestion);
