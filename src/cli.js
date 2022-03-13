02f757ade78eb05822e083b8445bb419d5aaea47
welberthenrique <welberthenrique@gmail.com>
2022-03-13 19:16:58 -0300

    Adicionado codigo da função para formatar preço juntamente com o teste

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,54 @@
+const {
+  listarProdutos,
+  listarCategorias,
+  listarCupons,
+} = require("./api-service");
+const { formatarValor } = require("./objetos");
+
 async function processarOpcao(opcao) {
   // TODO
-  console.log(opcao);
+  console.log(`A opção digitada foi: ${opcao}`);
+  let retorno;
+  let produtos;
+  let categorias;
+  let ProdutoFormatado;
+
+  switch (opcao.toUpperCase()) {
+    case "PRODUTOS":
+      retorno = await listarProdutos();
+      break;
+
+    case "PRODUTOS-FORMATADOS":
+      retorno = formataValorProdutos(await listarProdutos());
+
+      // retorno = ProdutoFormatado;
+      // console.log(retorno);
+      break;
+
+    case "DESCONTOS":
+      produtos = formataValorProdutos(await listarProdutos());
+      categorias = await listarCategorias();
+
+      retorno = produtos = produtos.map((p) => {
+        const categoria = categorias.find((c) => p.categoria === c.nome);
+
+        return { ...p, desconto: categoria?.desconto || 0 };
+      });
+
+      break;
+
+    // case "CUPONS":
+    //   retorno = await listarCupons();
+    //   break;
+
+    default:
+      throw new Error(`Opção inválida: ${opcao}`);
+  }
+  return retorno;
+}
+
+function formataValorProdutos(produtos) {
+  return produtos.map((p) => ({ ...p, preco: formatarValor(p.preco) }));
 }
 
 async function run() {
