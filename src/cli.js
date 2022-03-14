f3c48de40c0cd86a7aed3b5d7ff331287685d106
joselazarojunior <j.lazaro.junior@gmail.com>
2022-03-14 08:59:37 -0300

    A parte Desejáveis

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,44 @@
+/* eslint-disable no-mixed-spaces-and-tabs */
+const { listarProdutos, listarCategorias} = require("./api-service");
+const { formatarValor } = require("./objetos");
+
 async function processarOpcao(opcao) {
-  // TODO
-  console.log(opcao);
+  let retorno;
+  let produtos, categorias;
+
+	console.log(`A opção Digitada foi: ${ opcao }`);
+	
+  
+  switch (opcao.toUpperCase()) {
+	  case "PRODUTOS":
+	      retorno = await listarProdutos();
+	      break;
+    case "CATEGORIAS":
+          retorno = await listarCategorias();
+        break;
+    case "PRODUTOS-FORMATADOS":
+         retorno =  formataValorProdutos ( await listarProdutos() );
+        break;          
+	  
+    case "DESCONTOS":
+      produtos = formataValorProdutos(await listarProdutos());
+        categorias = await listarCategorias();
+
+      retorno = produtos.map((p) => {
+        const categoria = categorias.find((c) => p.categoria === c.nome);
+        return { ...p, desconto: categoria?.desconto || 0 };
+      });
+        break;
+                   
+    default:
+	     throw new Error ((opcao === "") ? "Informe uma opção." : `Opção inválida: ${ opcao }`);	      
+        
+	}
+  return retorno;
+}
+
+function formataValorProdutos(produtos) {
+  return produtos.map((p) => ({ ...p, preco: formatarValor(p.preco) }));
 }
 
 async function run() {
