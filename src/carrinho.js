c69dee81017b297dff77843af108b368916bf1dc
Glauber S Santana <glauber.silva14@gmail.com>
2022-03-14 17:55:04 -0300

    add: solutions and tests for challenge

--- src/carrinho.js
+++ src/carrinho.js
@@ -1,66 +1,111 @@
 const { GET } = require("./api-service");
-const readline = require("readline");
+const { askQuestion } = require("./ask-question");
 
-// const carrinhoDeCompras = [];
-
-const askQuestion = function (query) {
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
-
-  return new Promise((resolve) =>
-    rl.question(query, (ans) => {
-      rl.close();
-      resolve(ans);
-    })
-  );
-};
-
-const exibirMenu = function () {
+function imprimirOpcoes() {
   console.log("Escolha uma opção:");
-  console.log("1 - Listar Produtos");
-  console.log("2 - Incluir Produto no Carinho");
-  console.log("3 - Visualizar Carrinho");
-  console.log("4 - Finalizar Compra");
-  console.log("5 - Sair");
+  console.log("1 - Listar produtos");
+  console.log("2 - Incluir produto no carrinho");
+  console.log("3 - Visualizar carrinho");
+  console.log("4 - Finalizar compra");
+  console.log("x - Sair");
+}
+
+const escolherProduto = async function () {
+  const codProduto = await askQuestion(
+    "Qual código de produto deseja incluir no carrinho?"
+  );
+  const produtos = await GET("produtos");
+  const produto = produtos[+codProduto];
+  if (!produto) {
+    console.error(`Produto não localizado: ${codProduto}`);
+    return escolherProduto();
+  }
+  return produto;
 };
 
-const processarOpcao = async function (opcao) {
-  let escolhaProduto;
+const quantidadeProduto = async function () {
+  const qtd = await askQuestion("Digite a quantidade a ser adicionada: ");
+  if (!qtd) {
+    console.error(`Quantidade inválida: ${qtd}`);
+    return quantidadeProduto();
+  }
+  return qtd;
+};
+
+const incluirProduto = function (produto, qtd) {
+  carrinho.push({ ...produto, qtd, valor: produto.preco * qtd });
+  return carrinho;
+};
+
+const criarPromo = async function () {
+  const cupons = await GET("cupons");
+  const randPos = Math.round(Math.random() * (1 - 0) + 0);
+  const desconto = Math.round(Math.random() * (25 - 10) + 10);
+  return { nome: cupons[randPos], desconto: desconto };
+};
+
+const calculaTotal = function (precoParcial, cupom) {
+  return Math.round(precoParcial - (precoParcial * cupom.desconto) / 100);
+};
+
+const finalizar = async function (carrinho) {
+  if (carrinho.length === 0) {
+    console.error("O carrinho está vazio, você deve adicionar ao menos um item.");
+    return imprimirOpcoes();
+  } else {
+    const precoParcial = carrinho.reduce((a, b) => a + b.valor, 0);
+    console.log(`Subtotal: R$${precoParcial},00`); 
+    const cupom = await criarPromo();
+    console.log(`CUPOM: ${cupom.nome} : DESCONTO: ${cupom.desconto}`);
+    const total = calculaTotal(precoParcial, cupom);
+    console.log(`Total: R$${total},00`);
+  }
+  return processarOpcao("x");
+};
+
+let carrinho = [];
+
+async function processarOpcao(opcao) {
   switch (opcao) {
     case "1":
+      console.log("Lista de Produtos");
       console.table(await GET("produtos"));
       break;
-    case "2":
-      {
-        escolhaProduto = await askQuestion(
-          "Qual produto você deseja incluir ?"
-        );
-        const produtos = await GET("produtos");
-        const produto = produtos[escolhaProduto];
-        if (!produto) {
-          console.error(`O produto escolhido não existe:${escolhaProduto}`);
-        }
-      }
+    case "2": {
+      const produto = await escolherProduto();
+      let qtd = await quantidadeProduto();
+      const test = incluirProduto(produto, +qtd);
+      console.table(test);
       break;
+    }
+    case "3":
+      console.table(carrinho);
+      break;
+    case "4":
+      finalizar(carrinho);
+      break;
+    case "x":
+      return process.exit();
     default:
-      "thats an error";
-      break;
+      return console.log("Favor selecionar uma opção válida.");
   }
-};
+}
 
 async function run() {
-  exibirMenu();
+  imprimirOpcoes();
   let opcao;
   do {
     opcao = await askQuestion("Escolha uma opção: ");
-    processarOpcao(opcao);
-  } while (opcao !== 5);
+    await processarOpcao(opcao);
+  } while (opcao.toUpperCase() !== "X");
 }
 
 if (require.main === module) {
   run();
 }
 
-module.exports = { exibirMenu, processarOpcao };
+module.exports = {
+  imprimirOpcoes,
+  processarOpcao,
+  escolherProduto,
+};
--- src/carrinho.test.js
+++ src/carrinho.test.js
@@ -1,28 +1,36 @@
-const { exibirMenu } = require("./carrinho");
-const { processarOpcao } = require("./carrinho");
+const {
+  imprimirOpcoes,
+  processarOpcao,
+  escolherProduto,
+} = require("./carrinho");
 const { GET } = require("./api-service");
 
-const PRODUTOS_MOCK = require("../mocks/produtos.json");
+const { askQuestion } = require("./ask-question");
+jest.mock("./ask-question");
 jest.mock("./api-service");
+
+const PRODUTOS_MOCK = require("../mocks/produtos.json");
 
 describe("Desafio", () => {
   it("Deve imprimir corretamente as opções do menu.", () => {
     console.log = jest.fn();
-    exibirMenu();
+    imprimirOpcoes();
     expect(console.log.mock.calls).toEqual([
       ["Escolha uma opção:"],
-      ["1 - Listar Produtos"],
-      ["2 - Incluir Produto no Carinho"],
-      ["3 - Visualizar Carrinho"],
-      ["4 - Finalizar Compra"],
-      ["5 - Sair"],
+      ["1 - Listar produtos"],
+      ["2 - Incluir produto no carrinho"],
+      ["3 - Visualizar carrinho"],
+      ["4 - Finalizar compra"],
+      ["x - Sair"],
     ]);
   });
 
-  it("Deve listar os produtos, quando a opção escolhida for 1.", async () => {
+  it("Deve listar os produtos, quando a opção escolhida for 1", async () => {
+    console.log = jest.fn();
     console.table = jest.fn();
     GET.mockResolvedValue(PRODUTOS_MOCK);
     await processarOpcao("1");
+    expect(console.log.mock.calls).toEqual([["Lista de Produtos"]]);
     expect(console.table.mock.calls).toEqual([
       [
         [
@@ -35,6 +43,20 @@ describe("Desafio", () => {
       ],
     ]);
   });
-});
 
-processarOpcao("1");
+  it("Deve incluir os produtos quando digitar 2.", async () => {
+    GET.mockResolvedValue(PRODUTOS_MOCK);
+    console.log = jest.fn();
+    console.table = jest.fn();
+    askQuestion.mockResolvedValueOnce("2");
+    askQuestion.mockResolvedValueOnce("2");
+    await processarOpcao("2");
+    expect(console.table.mock.calls).toEqual([
+      [[{ categoria: "Bebida", nome: "Cerveja", preco: 7, qtd: 2, valor: 14 }]],
+    ]);
+  });
+
+  it("Deve sair da cli quando a opção digitada for x", async () => {
+    expect(processarOpcao("x")).toReturn(process.exit);
+  });
+});
