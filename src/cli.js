66314afa4b6a46361e8fd746395d1f59a0c36877
CatiaLarissa <catialarissaramos@gmail.com>
2022-03-14 17:31:27 -0300

    Novo commit
    Adicionando o commit do cli

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,38 @@
+const { listarProdutos, listarCategorias } = require("./api-service");
+const { formatarValor } = require("./objetos");
+
 async function processarOpcao(opcao) {
-  // TODO
-  console.log(opcao);
+  console.log(`A opção digitada foi: ${opcao}`);
+  let retorno;
+  let produtos;
+  let categorias;
+
+  switch (opcao.toUpperCase()) {
+    case "PRODUTOS":
+      retorno = await listarProdutos();
+      break;
+    case "PRODUTOS-FORMATADOS":
+      retorno = formataValorProdutos(await listarProdutos());
+      break;
+    case "CATEGORIAS":
+      retorno = listarCategorias();
+      break;
+    case "DESCONTOS":
+      produtos = formataValorProdutos(await listarProdutos());
+      categorias = await listarCategorias();
+      retorno = produtos.map((p) => {
+        const categoria = categorias.find((c) => p.categoria === c.nome);
+        return { ...p, desconto: categoria?.desconto || 0 };
+      });
+      break;
+    default:
+      throw new Error(`Opção inválida: ${opcao}`);
+  }
+  return retorno;
+}
+
+function formataValorProdutos(produtos) {
+  return produtos.map((p) => ({ ...p, preco: formatarValor(p.preco) }));
 }
 
 async function run() {
@@ -14,5 +46,5 @@ if (require.main === module) {
 }
 
 module.exports = {
-  processarOpcao,
+  processarOpcao, formataValorProdutos
 };
