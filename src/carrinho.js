00e17422935828ac84ec355a5614a6b62c945b01
ricardo <rosastaricardo84@gmail.com>
2022-03-14 01:04:57 -0300

    Revert "desafio finalizado"
    This reverts commit 5302082f6919c8b67d4093637f015e71bfb0f254.

--- src/carrinho.js
+++ src/carrinho.js
@@ -10,126 +10,55 @@ function imprimirOpcoes() {
   console.log("x - Sair");
 }
 
-function incluiDescontoProdutos(produtos) {
-
-    let desconto;
-    
-     return produtos.map(item => {
-
-        if(item.categoria === "Alimentação") desconto = 30;
-        else if(item.categoria === "Infantil") desconto = 15;
-        else desconto = 0;
-        
-        return {...item , desconto: desconto};
-      });
-
-}
-
 let carrinho = [];
 
-
-
 async function processarOpcao(opcao) {
-  let codigoProduto, produtos, produto, quantidade, visalizaProdutoIncluidoCarrinho;
-  const produtosComDesconto = incluiDescontoProdutos(await listarProdutos());
-  let subtotal, digitarCupom, digitarDescontoCupom, total;
+  let codProduto, produtos, produto, qtd;
 
   switch (opcao) {
    
     case "1":
       console.log("Lista de Produtos");
-      console.table(produtosComDesconto);
+      console.table(await listarProdutos());
       break;
    
       case "2":
-      codigoProduto = await askQuestion(
+      codProduto = await askQuestion(
         "Qual código de produto deseja incluir no carrinho?"
       );
-      codigoProduto = parseInt(codigoProduto);
-      produtos = produtosComDesconto;
-      produto = produtos[codigoProduto];
+      codProduto = parseInt(codProduto);
+      produtos = await listarProdutos();
+      produto = produtos[codProduto];
 
       if (!produto) {
-        console.error(`Produto não localizado: ${codigoProduto}`);
+        console.error(`Produto não localizado: ${codProduto}`);
         return;
       }
 
-      quantidade = await askQuestion(
+      qtd = await askQuestion(
         "Quantas unidades deseja adicionar ao carrinho?"
       );
-      quantidade = parseInt(quantidade);
+      qtd = parseInt(qtd);
 
-      if (!quantidade) {
-        console.error(`Quantidade inválida: ${quantidade}`);
+      if (!qtd) {
+        console.error(`Quantidade inválida: ${qtd}`);
         return;
       }
 
-      visalizaProdutoIncluidoCarrinho = { ...produto, quantidade, valor: +((produto.preco * quantidade) * (1 - (produto.desconto/100))).toFixed(2) };
-
-      carrinho.push(visalizaProdutoIncluidoCarrinho);
-
-      console.table([visalizaProdutoIncluidoCarrinho]);
-      console.log("Produto incluído com sucesso no carrinho.");
-      
-      break;
-    
-      case "3":
-
-      console.log("Carrinho de Compras");
+      carrinho.push({ ...produto, qtd, valor: produto.preco * qtd });
 
       console.table(carrinho);
-     
       break;
-      
-      
-      
-      case "4":
-        
-        console.log("Concluir compra");
-        console.table(carrinho);
-        
-        subtotal = carrinho.reduce((acc , item) => acc + item.valor , 0);
-        
-        console.log(`Subtotal: R$ ${subtotal.toFixed(2)}`);
-        digitarCupom = await askQuestion("Qual o nome do cupom?");
-        digitarDescontoCupom = await askQuestion("Qual o desconto do cupom?");
-
-        if(digitarCupom.trim().toUpperCase() === "ALURANU" || digitarCupom.trim().toUpperCase() === "NULABSSA" ) {
-          
-          total = subtotal * (1 - +(digitarDescontoCupom.trim()/100));
-        }else{
-
-          total = subtotal;
-            
-        }
-
-        console.log(`Total: R$ ${total.toFixed(2)}`);
-        console.log("Compra finalizada com sucesso!");
-
-
-
-      break;
-
-      default:
-
-      if(opcao.toUpperCase() !== "X") console.log("Digite uma opção válida!");
-      
+    default:
       break;
   }
-
-  if (require.main === module && opcao.toUpperCase() !== "X")  imprimirOpcoes();
 }
 
-
-
 async function run() {
-  
   imprimirOpcoes();
-  
   let opcao;
-  
   do {
-    opcao = await askQuestion("Opção:");
+    opcao = await askQuestion("Escolha uma opção:");
     await processarOpcao(opcao);
   } while (opcao.toUpperCase() !== "X");
 }
@@ -142,6 +71,5 @@ if (require.main === module) {
 
 module.exports = {
   imprimirOpcoes,
-  processarOpcao,
-  carrinho
+  processarOpcao
 };
