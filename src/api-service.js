ed044524cb3ce1eb8b0a281b174fbc2bf65571ab
Janaise Vieira <janavieiraa@hotmail.com>
2022-03-14 17:29:46 -0300

    Atividades essenciais e desejáveis

---
+++ src/api-service.js
@@ -0,0 +1,34 @@
+const fetch = require("node-fetch");
+
+async function listarProdutos(){
+    const response = await fetch("http://stupefied-kelle-a2c79e.netlify.app/produtos.json");
+    verificarErros(response);
+    const data = await response.json();
+    return data;
+}
+
+function verificarErros(response){
+    if (response.status !== 200) {
+        throw new Error(`${response.statusText}: ${response.status}`);
+    }
+}
+
+async function listarCategorias(){
+    const response = await fetch("http://stupefied-kelle-a2c79e.netlify.app/categorias.json");
+    verificarErros(response);
+    const data = await response.json();
+    return data;
+}
+
+async function listarCuponsValidos(){
+    const response = await fetch("https://stupefied-keller-a2c79e.netlify.app/cupons.json");
+    verificarErros(response);
+    const data = await response.json();
+    return data;
+}
+
+module.exports = {
+    listarProdutos, 
+    listarCategorias,  
+    listarCuponsValidos,
+};
--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,44 @@
+const { listarProdutos, listarCategorias } = require("./api-service");
+const { formatarValor } = require("./objetos");
+
 async function processarOpcao(opcao) {
-  // TODO
-  console.log(opcao);
+  console.log(`A opção digitada foi: ${opcao}`);
+
+  let retorno;
+  let produtos;
+  let categorias;
+
+  if (opcao == "" || opcao == undefined){
+    throw new Error("Favor informar uma opção");
+  }
+
+  switch (opcao.toUpperCase()){
+    case "PRODUTOS":
+      retorno = await listarProdutos();
+      break;
+    case "DESCONTOS":
+      produtos = formataValorProdutos(await listarProdutos());
+      categorias = await listarCategorias();
+      
+      retorno = produtos.map((p1) => {
+        const categoria = categorias.find((c1) => p1.categoria === c1.nome);
+        return {...p1, desconto: categoria?.desconto || 0};
+      });   
+      break;
+    case "CATEGORIAS":
+      retorno = await listarCategorias();
+      break;
+    case "PRODUTOS-FORMATADOS":
+      retorno = formataValorProdutos(await listarProdutos());
+      break;
+    default:
+      throw new Error(`Opção inválida: ${opcao}`);
+  }
+  return retorno;
+}
+
+function formataValorProdutos(produtos) {
+  return produtos.map((p) => ({ ...p, preco: formatarValor(p.preco) }));
 }
 
 async function run() {
