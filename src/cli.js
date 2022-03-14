559925820f32362b4b672a9e207940404ed9d156
MarckoAj <96995466+MarckoAj@users.noreply.github.com>
2022-03-14 15:08:06 -0300

    desejavel concluido

--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,45 @@
+const { listarProdutos, listarCategorias, } = require("./api-service");
+const { formatarValor } = require("./objetos");
+
+const formataValoresDeProdutos = produtos => produtos.map((p) => ({ ...p, preco: formatarValor(p.preco) }));
+
+
 async function processarOpcao(opcao) {
-  // TODO
-  console.log(opcao);
+  const opcoesValidas = ["produtos", "produtos-formatados", "categorias", "descontos"];
+  let valorDeRetorno;
+
+  if (!opcoesValidas.includes(opcao)) {
+    throw new Error((opcao === undefined) ? "Informe uma opção." : `Opção inválida: ${opcao}`);
+  }
+
+  if (opcao === opcoesValidas[0]) {
+    valorDeRetorno = await listarProdutos();
+  }
+
+  if (opcao === opcoesValidas[1]) {
+    const listaDeProdutos = await listarProdutos();
+    valorDeRetorno = formataValoresDeProdutos(listaDeProdutos);
+  }
+
+  if (opcao === opcoesValidas[2]) {
+    valorDeRetorno = await listarCategorias();
+  }
+
+  if (opcao === opcoesValidas[3]) {
+    const listaDeProdutos = await listarProdutos();
+    const listaDeCategorias = await listarCategorias();
+
+    const listaComDescontos = listaDeProdutos.map(p => {
+      const categoria = listaDeCategorias.find(c => p.categoria === c.nome);
+      return { ...p, desconto: categoria?.desconto || 0 };
+    });
+
+    valorDeRetorno = formataValoresDeProdutos(listaComDescontos);
+  }
+
+  return valorDeRetorno;
 }
+
 
 async function run() {
   const opcao = process.argv[2];
@@ -16,3 +54,4 @@ if (require.main === module) {
 module.exports = {
   processarOpcao,
 };
+
