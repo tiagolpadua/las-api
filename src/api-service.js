9582370e11dcb53ecca2206d08e319c3f7823846
Tauane Sales <taysales6@gmail.com>
2022-03-14 21:31:59 -0300

    lidando com erros

--- src/api-service.js
+++ src/api-service.js
@@ -1,41 +1,41 @@
 const fetch = require("node-fetch");
-const {incluirPrecoFormatado } = require("./objetos");
-const {obterDescontoCategoria } = require("./objetos");
+const { incluirPrecoFormatado } = require("./objetos");
+const { obterDescontoCategoria } = require("./objetos");
 
 
 // Crie uma função e o teste desta função, que lista os produtos a partir da API e retorna um JSON
-  // com esta lista de produtos
-async function listarProdutos(){
+// com esta lista de produtos
+async function listarProdutos() {
     const listagemProdutos = await fetch("https://stupefied-keller-a2c79e.netlify.app/produtos.json");
-    if(listagemProdutos.status !== 200){
-     throw new Error(`${listagemProdutos.statusText}: ${listagemProdutos.status}`);
- }
+    if (listagemProdutos?.status !== 200) {
+        throw new Error(`${listagemProdutos?.statusText}: ${listagemProdutos?.status}`);
+    }
     const data = await listagemProdutos.json();
- 
-   return data;
- }
- 
- async function listarCategoria(){
-     const listagemCategoria = await fetch("https://stupefied-keller-a2c79e.netlify.app/categorias.json");
-     if(listagemCategoria.status !== 200){
-         throw new Error(`${listagemCategoria.statusText}: ${listagemCategoria.status}`);
-     }
-     const data = await listagemCategoria.json();
- 
-     return data;
- }
- 
- async function listarCupom(){
-     const listagemCupom = await fetch("https://stupefied-keller-a2c79e.netlify.app/cupons.json");
-     if(listagemCupom.status !== 200){
-         throw new Error(`${listagemCupom.statusText}: ${listagemCupom.status}`);
-     }
-     const data = await listagemCupom.json();
-         
-     return data;
- }
- 
- async function tableFormatPrice() {
+
+    return data;
+}
+
+async function listarCategoria() {
+    const listagemCategoria = await fetch("https://stupefied-keller-a2c79e.netlify.app/categorias.json");
+    if (listagemCategoria.status !== 200) {
+        throw new Error(`${listagemCategoria.statusText}: ${listagemCategoria.status}`);
+    }
+    const data = await listagemCategoria.json();
+
+    return data;
+}
+
+async function listarCupom() {
+    const listagemCupom = await fetch("https://stupefied-keller-a2c79e.netlify.app/cupons.json");
+    if (listagemCupom.status !== 200) {
+        throw new Error(`${listagemCupom.statusText}: ${listagemCupom.status}`);
+    }
+    const data = await listagemCupom.json();
+
+    return data;
+}
+
+async function tableFormatPrice() {
     const data = await listarProdutos();
 
     return data.map(format => {
@@ -43,14 +43,14 @@ async function listarProdutos(){
 
         productF["preco"] = productF["precoFormatado"];
         delete productF.precoFormatado;
-       
+
         return productF;
 
-       
+
     });
 
- }
- async function listarDescCategoria() {
+}
+async function listarDescCategoria() {
     const data = await listarProdutos();
 
     return data.map(format => {
@@ -59,20 +59,18 @@ async function listarProdutos(){
         productF["preco"] = productF["precoFormatado"];
         productF["desconto"] = obterDescontoCategoria(productF["categoria"]);
         delete productF.precoFormatado;
-       
+
         return productF;
 
-       
+
     });
 
- }
- tableFormatPrice();
- listarDescCategoria();
- 
- module.exports = {
-     listarProdutos,
-     listarCategoria,
-     listarCupom,
-     tableFormatPrice, 
-     listarDescCategoria, 
- };
+}
+
+module.exports = {
+    listarProdutos,
+    listarCategoria,
+    listarCupom,
+    tableFormatPrice,
+    listarDescCategoria,
+};
--- src/cli.js
+++ src/cli.js
@@ -3,64 +3,63 @@ const {
   listarCategoria,
   tableFormatPrice,
   listarDescCategoria
- } = require("./api-service");
-  
- async function processarOpcao(opcao) {
-  const [produtos, categorias, produtosFormatados, descontos] =
-    await Promise.all([
-      listarProdutos(),
-      listarCategoria(),
-      tableFormatPrice(),
-      listarDescCategoria()
-    ]);
-  
-    // switch (opcao) {
-    //   case "produtos":
-    //     return produtos;
-    //   case "categorias":
-    //     return categorias;
-    //   case "produtos-formatados":
-    //     return produtosFormatados;
-    //   case "descontos":
-    //     return descontos;
-    //   case undefined:
-    //     throw new Error("Informe uma opção");
-    //   default:
-    //     throw new Error(`Opção inválida: ${opcao}`);
-    // }
+} = require("./api-service");
 
-    if ((opcao) === "produtos") {
-      return produtos;
-    }
-    if ((opcao) === "categorias") {
-      return categorias;
-    }
-    if ((opcao) === "descontos") {
-      return descontos;
-    }
-    if ((opcao) === "produtos-formatados") {
-      return produtosFormatados;
-    }
-    if ((opcao) === undefined) {
-      throw new Error("Informe uma opção");
-    }
-    else{
-      throw new Error(`Opção inválida: ${opcao}`);
-    }
+async function processarOpcao(opcao) {
+  try {
+    var [produtos, categorias, produtosFormatados, descontos] =
+    await Promise.all([listarProdutos(), listarCategoria(), tableFormatPrice(), listarDescCategoria()]);
+  } catch (err){
+    console.error(err.message);
+  }
   
- }
-  
- async function run() {
+
+  // switch (opcao) {
+  //   case "produtos":
+  //     return produtos;
+  //   case "categorias":
+  //     return categorias;
+  //   case "produtos-formatados":
+  //     return produtosFormatados;
+  //   case "descontos":
+  //     return descontos;
+  //   case undefined:
+  //     throw new Error("Informe uma opção");
+  //   default:
+  //     throw new Error(`Opção inválida: ${opcao}`);
+  // }
+
+  if ((opcao) === "produtos") {
+    return produtos;
+  }
+  if ((opcao) === "categorias") {
+    return categorias;
+  }
+  if ((opcao) === "descontos") {
+    return descontos;
+  }
+  if ((opcao) === "produtos-formatados") {
+    return produtosFormatados;
+  }
+  if ((opcao) === undefined) {
+    throw new Error("Informe uma opção");
+  }
+  else {
+    throw new Error(`Opção inválida: ${opcao}`);
+  }
+
+}
+
+async function run() {
   const opcao = process.argv[2];
   const saida = await processarOpcao(opcao);
   console.table(saida);
- }
-  
- if (require.main === module) {
+}
+
+if (require.main === module) {
   run();
- }
-  
- module.exports = {
+}
+
+module.exports = {
   processarOpcao,
- };
- 
+};
