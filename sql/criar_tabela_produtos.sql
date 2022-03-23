b4c542e0873b13656488748bf33d40ef028e2066
welberthenrique <welberthenrique@gmail.com>
2022-03-23 12:23:40 -0300

    Adicionado script criar_tabela_produtos.sql

--- sql/criar_tabela_produtos.sql
+++ sql/criar_tabela_produtos.sql
@@ -1,1 +1,12 @@
-SELECT 'SUBTITUA O CONTEÚDO DESTE ARQUIVO PELA SUA RESPOSTA :-)';
+--// Escreva um SQL que cria a tabela Produtos, cada produto 
+-- possui um id (inteiro auto incremento), um nome (texto não nulo) 
+-- e um id de categoria obrigatório (inteiro) e um preço obrigatório (decimal)
+
+
+CREATE TABLE produtos(
+ID INTEGER  PRIMARY KEY AUTOINCREMENT, 
+NOME TEXT NOT NULL,
+ID_CATEGORIA REAL NOT NULL,
+--FOREIGN KEY(ID_CATEGORIA) REFERENCES categorias(ID)
+PRECO REAL NOT NULL
+);
