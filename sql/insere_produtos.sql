107259797a0d5edbaa2bc91802c53066b4885759
welberthenrique <welberthenrique@gmail.com>
2022-03-23 13:13:43 -0300

    Implementado script insere_produtos.sql

--- sql/insere_produtos.sql
+++ sql/insere_produtos.sql
@@ -1,1 +1,7 @@
-SELECT 'SUBTITUA O CONTEÚDO DESTE ARQUIVO PELA SUA RESPOSTA :-)';
+--// Nome: Coca-cola, Categoria: 2, Preço: 5,50
+  --// Nome: Serpentina, Categoria: 1, Preço: 3,00
+
+  INSERT INTO produtos(NOME, ID_CATEGORIA, PRECO)
+  VALUES
+  ('Coca-cola',2, 5.50),
+  ('Serpentina',1,3.00)
