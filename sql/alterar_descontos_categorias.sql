dfc0cd15200d474c3ae4eeedf591520694a805b9
welberthenrique <welberthenrique@gmail.com>
2022-03-23 13:19:44 -0300

    Implementado script alterar_descontos_categorias.sql

--- sql/alterar_descontos_categorias.sql
+++ sql/alterar_descontos_categorias.sql
@@ -1,1 +1,4 @@
-SELECT 'SUBTITUA O CONTEÚDO DESTE ARQUIVO PELA SUA RESPOSTA :-)';
+--// Escreva um SQL que altera o desconto de todas as categorias com desconto de 10% para 20%
+
+UPDATE CATEGORIAS set DESCONTO = 20
+WHERE DESCONTO = 10 
