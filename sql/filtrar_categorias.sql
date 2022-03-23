615222a85172006359e0936d951ff1b2a183672d
welberthenrique <welberthenrique@gmail.com>
2022-03-23 14:40:18 -0300

    Implementado script filtrar_categorias.sql

--- sql/filtrar_categorias.sql
+++ sql/filtrar_categorias.sql
@@ -1,1 +1,4 @@
-SELECT 'SUBTITUA O CONTEÚDO DESTE ARQUIVO PELA SUA RESPOSTA :-)';
+--// Escreva um SQL que recupere os nomes das categorias com desconto entre 5% e 25%
+
+SELECT NOME FROM CATEGORIAS
+WHERE DESCONTO BETWEEN 5 AND 25
