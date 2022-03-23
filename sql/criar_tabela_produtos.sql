74a06b6eeeb6c8bb852a584a88d75ee06598c266
welberthenrique <welberthenrique@gmail.com>
2022-03-23 12:25:56 -0300

    Finalizado etapa Essencial

--- sql/criar_tabela_produtos.sql
+++ sql/criar_tabela_produtos.sql
@@ -1,8 +1,3 @@
---// Escreva um SQL que cria a tabela Produtos, cada produto 
--- possui um id (inteiro auto incremento), um nome (texto não nulo) 
--- e um id de categoria obrigatório (inteiro) e um preço obrigatório (decimal)
-
-
 CREATE TABLE produtos(
 ID INTEGER  PRIMARY KEY AUTOINCREMENT, 
 NOME TEXT NOT NULL,
