231b2365d603868ff73b5a64971740a5ba9415db
Domingos Alcantara <domingos_alcantara@yahoo.com.br>
2022-03-24 19:47:33 -0300

    Comitando o Essencial desta semana, com os erros corrigidos

--- sql/insere_produtos.sql
+++ sql/insere_produtos.sql
@@ -1,1 +1,6 @@
-SELECT 'SUBTITUA O CONTEÚDO DESTE ARQUIVO PELA SUA RESPOSTA :-)';
+INSERT INTO produtos 
+	(nome, id_categoria, preco)
+VALUES
+	("Coca-cola", 2, 5.50),
+    ("Serpentina", 1, 3.00)
+;
