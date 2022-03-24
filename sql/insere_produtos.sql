729c7776726d4864caa041aa139b86d31b7b446e
CatiaLarissa <catialarissaramos@gmail.com>
2022-03-24 10:51:00 -0300

    Add files via upload

--- sql/insere_produtos.sql
+++ sql/insere_produtos.sql
@@ -1,4 +1,3 @@
 
-INSERT INTO produtos (ID_CATEGORIA, NOME, PRECO) VALUES (2, "Coca-cola",5.50);
-INSERT INTO produtos (ID_CATEGORIA, NOME, PRECO) VALUES (1,"Serpentina", 3);
+INSERT INTO produtos (ID, ID_CATEGORIA, NOME, PRECO) VALUES (1, 2, "Coca-cola",5.50), (2, 1,"Serpentina", 3);
 
