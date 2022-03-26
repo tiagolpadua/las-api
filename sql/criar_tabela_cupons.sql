8d8ee586da3b922630b5faf53dfb7c862c343f01
amandassa <amandassa504@gmail.com>
2022-03-26 15:38:38 -0300

    Queries desafio S14

--- sql/criar_tabela_cupons.sql
+++ sql/criar_tabela_cupons.sql
@@ -1,1 +1,7 @@
-SELECT 'SUBTITUA O CONTEÚDO DESTE ARQUIVO PELA SUA RESPOSTA :-)';
+/* Escreva um SQL que cria a tabela Cupons, cada cupom possui um id (inteiro auto incremento), 
+um nome (texto não nulo) e um desconto associado (valor decimal não nulo)*/
+CREATE TABLE Cupons (
+    ID INTEGER PRIMARY KEY AUTOINCREMENT,
+    NOME TEXT NOT NULL,
+    DESCONTO REAL NOT NULL
+);
