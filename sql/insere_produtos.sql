ed2cfc8f457d3d400f2b51a988505a3ec5a07be6
albertconceicao <albertconceicao2@gmail.com>
2022-03-28 11:21:05 -0300

    refatorando código

--- sql/insere_produtos.sql
+++ sql/insere_produtos.sql
@@ -1,3 +1,2 @@
-INSERT INTO PRODUTOS (ID_CATEGORIA, NOME, PRECO) 
-    VALUES (2,"Coca-cola", 5.5) ,(1,"Serpentina", 3)
+INSERT INTO PRODUTOS (ID_CATEGORIA, NOME, PRECO) VALUES (2,"Coca-cola", 5.5) ,(1,"Serpentina", 3);
     ;
