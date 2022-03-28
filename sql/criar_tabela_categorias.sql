0a15eaa44e6f09e37cb90e643029138d43bebdc4
Matheus Rocha <65717646+imRochaMatheus@users.noreply.github.com>
2022-03-27 22:03:04 -0300

    Finalizando desafio semana 14

--- sql/criar_tabela_categorias.sql
+++ sql/criar_tabela_categorias.sql
@@ -1,1 +1,7 @@
-SELECT 'SUBTITUA O CONTEÚDO DESTE ARQUIVO PELA SUA RESPOSTA :-)';
+CREATE TABLE Categorias(
+	ID int identity primary key,
+    NOME text not null,
+    DESCONTO decimal
+);
+
+SELECT Categorias
