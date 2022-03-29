22a658bde3c0b86ef885d6fd14060ae99b9d196c
Leandro <leogrcel@gmail.com>
2022-03-28 22:27:42 -0300

    Alterar Descontos Categorias

--- sql/insere_produtos.sql
+++ sql/insere_produtos.sql
@@ -1,11 +1,3 @@
-INSERT INTO Produtos (
-    NOME, ID_CATEGORIA, PRECO
-) VALUES (
-    'Coca-cola', 2, 5.50
-);
+INSERT INTO Produtos (ID, ID_CATEGORIA, NOME, PRECO) VALUES (1, 2,"Coca-cola", 5.5);
 
-INSERT INTO Produtos (
-    NOME, ID_CATEGORIA, PRECO
-) VALUES (
-    'Serpentina', 1, 3.00
-);
+INSERT INTO Produtos (ID, ID_CATEGORIA, NOME, PRECO) VALUES (1, 1,"Serpentina", 3);
