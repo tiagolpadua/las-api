48ef9c1eefcf1dcce2c85d70a4ad367b2cb8e7f2
Glauber Santana <glauber.silva14@gmail.com>
2022-04-11 21:04:25 -0300

    add: added db connection

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const conn = require("./services/conexao");
 const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
   res.send("Olá Mundo!");
 });
+
+
 
 app.listen(port, () => {
   console.log(`LAS-API ouvindo na porta: ${port}`);
