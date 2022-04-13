d1c47ce46ab7ee77f92b0e796151889c5e1abb2b
Mau-Me <mauriciosm95@gmail.com>
2022-04-13 16:13:29 -0300

    Refactor: Checa a conexão com o bd
    Co-authored-by: tainahcarvalho21 <tainah_carvalho@hotmail.com>

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 const rotasUsuarios = require("./controllers/usuarios");
 const express = require("express");
+const conexao = require("./infraestrutura/conexao");
+const tabelas = require("./infraestrutura/tabelas");
 const app = express();
 const port = 3000;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-rotasUsuarios(app);
+conexao.connect((erro) => {
+  if (erro) {
+    console.log(erro);
+  } else {
+    tabelas.init(conexao);
+    console.log("Conexão com o BD realizada com sucesso");
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
+    rotasUsuarios(app);
 
-app.listen(port, () => {
-  console.log(`LAS-API ouvindo na porta: ${port}`);
+    app.listen(port, () => {
+      console.log(`LAS-API ouvindo na porta: ${port}`);
+    });
+  }
 });
