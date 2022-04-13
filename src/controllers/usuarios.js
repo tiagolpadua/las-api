e12be0a6f4e8b44c3876ee030d689544b7f376b5
Mau-Me <mauriciosm95@gmail.com>
2022-04-13 16:09:23 -0300

    Update: correção no nome da rota
    Co-authored-by: tainahcarvalho21 <tainah_carvalho@hotmail.com>

--- src/controllers/usuarios.js
+++ src/controllers/usuarios.js
@@ -1,11 +1,11 @@
 const Usuario = require("../models/usuarios");
 
 module.exports = (app) => {
-  app.get("/usuario", (req, res) => {
+  app.get("/usuarios", (req, res) => {
     Usuario.buscaTodos(res);
   });
 
-  app.get("/usuario/:id", (req, res) => {
+  app.get("/usuarios/:id", (req, res) => {
     const id = parseInt(req.params.id);
 
     Usuario.buscaPorId(id, res);
