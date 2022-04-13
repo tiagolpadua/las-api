32f777b55e0850d0c9af463508ed145442091452
Mau-Me <mauriciosm95@gmail.com>
2022-04-13 12:41:57 -0300

    Add: Funcs buscaTodos e buscaPorId
    Co-authored-by: tainahcarvalho21 <tainah_carvalho@hotmail.com>

---
+++ src/models/usuarios.js
@@ -0,0 +1,30 @@
+const conexao = require("../infraestrutura/conexao");
+
+class Usuario {
+  buscaTodos(res) {
+    const sql = "SELECT * FROM usuarios";
+
+    conexao.query(sql, (erro, resultados) => {
+      if (erro) {
+        res.status(400).json(erro);
+      } else {
+        res.status(200).json(resultados);
+      }
+    });
+  }
+
+  buscaPorId(id, res) {
+    const sql = `SELECT * FROM usuarios WHERE id=${id}`;
+
+    conexao.query(sql, (erro, resultados) => {
+      const usuario = resultados[0];
+      if (erro) {
+        res.status(400).json(erro);
+      } else {
+        res.status(200).json(usuario);
+      }
+    });
+  }
+}
+
+module.exports = new Usuario();
