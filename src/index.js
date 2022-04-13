04bcfce4f3fa17e78314d9555236610d7a147077
Mau-Me <mauriciosm95@gmail.com>
2022-04-13 12:48:17 -0300

    Refactor: Utiliza as rotas de usuários
    Co-authored-by: tainahcarvalho21 <tainah_carvalho@hotmail.com>

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
+const rotasUsuarios = require("./controllers/usuarios");
 const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Olá Mundo!");
-});
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+rotasUsuarios(app);
 
 app.listen(port, () => {
   console.log(`LAS-API ouvindo na porta: ${port}`);
