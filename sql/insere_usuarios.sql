3d878f21a621581accb00b90ad37df73d135ec37
Mau-Me <mauriciosm95@gmail.com>
2022-04-13 12:12:46 -0300

    Add: Scripts sql de criação do bd-tabelas e insert
    Co-authored-by: tainahcarvalho21 <tainah_carvalho@hotmail.com>

---
+++ sql/insere_usuarios.sql
@@ -0,0 +1,3 @@
+USE las;
+
+INSERT INTO usuarios(nome, urlFotoPerfil) VALUES('Tainah Carvalho', 'https://avatars.githubusercontent.com/u/83484176?v=4'), ('Mauricio Menezes', 'https://avatars.githubusercontent.com/u/20570844?v=4');
