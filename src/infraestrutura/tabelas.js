8dce0909b114a2e707cf573fe4199d5be42ca293
MoniqueSukerman <nique_sukerman@hotmail.com>
2022-04-13 19:00:15 -0300

    Update

---
+++ src/infraestrutura/tabelas.js
@@ -0,0 +1,22 @@
+class Tabelas {
+  init(conexao) {
+    this.conexao = conexao;
+
+    this.criarUsuario();
+  }
+
+  criarUsuario() {
+    const sql = `CREATE TABLE IF NOT EXISTS Usuario 
+        (id int NOT NULL AUTO_INCREMENT, nome varchar(50) NOT NULL, urlFotoPerfil text, PRIMARY KEY(id))`;
+
+    this.conexao.query(sql, (erro) => {
+      if (erro) {
+        console.log(erro);
+      } else {
+        console.log("Tabela Usuario criada com sucesso");
+      }
+    });
+  }
+}
+
+module.exports = new Tabelas();
