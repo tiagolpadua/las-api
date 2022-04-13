85120b11c91f3db53cfa80b4feb77f782ae258d9
Mau-Me <mauriciosm95@gmail.com>
2022-04-13 16:14:47 -0300

    Update: Testes essenciais realizados
    Co-authored-by: tainahcarvalho21 <tainah_carvalho@hotmail.com>

--- src/index.test.js
+++ src/index.test.js
@@ -1,6 +1,6 @@
-const conexao = undefined; // require("./infraestrutura/conexao");
-const Tabelas = undefined; // require("./infraestrutura/tabelas");
-const usuariosController = undefined; // require("./controllers/usuarios");
+const conexao = require("./infraestrutura/conexao");
+const Tabelas = require("./infraestrutura/tabelas");
+const usuariosController = require("./controllers/usuarios");
 const Usuario = undefined; // require("./models/usuarios");
 const nodemon = undefined; // require("nodemon");
 const consign = undefined; // require("consign");
