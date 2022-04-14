49ab37885ec634ea25b22ae6c4d25781cbf26687
Renata Santana <renatasantana114@gmail.com>
2022-04-14 18:21:56 -0300

    Atualizando a branch

--- src/controllers/usuarios.js
+++ src/controllers/usuarios.js
@@ -1,9 +1,11 @@
 const Usuarios = require("../models/usuarios");
 
 module.exports = (app) => {
-  app.get("/usuarios/:id", (req, res) => {
-    const id = parseInt(req.params.id);
-
-    Usuarios.buscaUsuario(id, res);
+  app.get("/usuarios/:id", (req, res) => res.send('Buscando usuário'))
+  
+  app.post("/usuarios/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  Usuarios.buscaUsuario(id, res);
   });
 };
+
