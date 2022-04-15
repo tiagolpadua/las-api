b6bd5034ec155b0093de5e2128372522ad6b0d2a
Ge vasconcelos <geminivasconceelos@gmail.com>
2022-04-14 22:06:00 -0300

    Refactor - refatoracao o index.js e passagem de testes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
-const express = require("express");
+const customExpress = require("./config/customExpress");
 const conexao = require("./infraestrutura/conexao");
-const app = express();
+const Tabelas = require("./infraestrutura/tabelas");
+// const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Olá Mundo!");
-});
+
+// app.get("/usuarios", (req, res) => {
+//   res.send("Olá Mundo!");
+// });
 
 conexao.connect((erro) => {
   if (erro) {
     console.log(erro);
   } else {
     console.log("conectado com sucesso");
+
+    const app = customExpress();
+
+    Tabelas.init(conexao);
 
     app.listen(port, () => {
       console.log(`LAS-API ouvindo na porta: ${port}`);
