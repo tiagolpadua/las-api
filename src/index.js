ac46340e19cb2c8a63648e8965831b3a4d94b63e
ricardo <rosastaricardo84@gmail.com>
2022-04-15 12:03:20 -0300

    config conexao banco
    Co-authored-by: Albert <albertoconceicao2@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
-const express = require("express");
-const app = express();
+const controllers = require("./servidor/infra");
+
+const app = controllers();
+
+const Tabelas = require("./infraestrutura/Tabelas");
+
+const conexao = require("./infraestrutura/conexao");
+
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Olá Mundo!");
-});
+conexao.connect((erro) => {
+  if (erro) console.log(erro);
+  else {
+    app.listen(port, () => {
+      console.log(`LAS rodando na porta ${port}`);
+    });
 
-app.listen(port, () => {
-  console.log(`LAS-API ouvindo na porta: ${port}`);
+    Tabelas.init(conexao);
+  }
 });
---
+++ src/servidor/infra.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const consign = require("consign");
+const bodyParser = require("body-parser");
+
+module.exports = () => {
+  const app = express();
+
+  app.set("json spaces", 4);
+
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(bodyParser.json());
+
+  consign({ verbose: true }).include("controllers").into(app);
+
+  return app;
+};
