eecb062dd1a1f0507ae7693cbda0b1505237b5e4
Ge vasconcelos <geminivasconceelos@gmail.com>
2022-04-14 22:06:47 -0300

    Refactor - correcao na criacao da tabela e conexao com banco dedados

--- src/infraestrutura/conexao.js
+++ src/infraestrutura/conexao.js
@@ -1,11 +1,11 @@
 const mysql = require("mysql");
 
 const conexao = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "las",
-    password: "admin",
-    database: "las"
+  host: "localhost",
+  port: 3306,
+  user: "las",
+  password: "admin",
+  database: "las",
 });
 
 module.exports = conexao;
--- src/infraestrutura/tabelas.js
+++ src/infraestrutura/tabelas.js
@@ -2,21 +2,21 @@ class Tabelas {
   init(conexao) {
     this.conexao = conexao;
 
-    this.criarUsuarios;
+    this.criarUsuarios();
   }
 
   criarUsuarios() {
     const sql =
-      "CREATE TABLE IF NOT EXISTS Usuarios (id int NOT NULL AUTO_INCREMENT, nome varchar(255) NOT NULL, urlFotoPerfil varchar(255))";
-  
-    this.conexao.query(sql, erro => {
-        if(erro){
-            console.log("erro");
-        }else{
-            console.log("Tabela usuarios criada com sucesso");
-        }
+      "CREATE TABLE IF NOT EXISTS Usuarios (id int NOT NULL AUTO_INCREMENT, nome varchar(50) NOT NULL, urlFotoPerfil varchar(100), PRIMARY KEY(id))";
+
+    this.conexao.query(sql, (erro) => {
+      if (erro) {
+        console.log("erro");
+      } else {
+        console.log("Tabela usuarios criada com sucesso");
+      }
     });
-   }
+  }
 }
 
-module.exports = new Tabelas;
+module.exports = new Tabelas();
