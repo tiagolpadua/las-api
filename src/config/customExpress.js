3e7008ea02c4edf1e0f4456d859ebd38e7006576
Ge vasconcelos <geminivasconceelos@gmail.com>
2022-04-14 22:04:52 -0300

    Feat - adicao da pasta config com o customExpress

---
+++ src/config/customExpress.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const consign = require("consign");
+
+module.exports = () => {
+    const app = express();
+    app.use(express.urlencoded({ extended: true }));
+    app.use(express.json());
+  
+    consign({ cwd: "src" }).include("controllers").into(app);
+  
+    return app;
+  };
