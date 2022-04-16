4322d51a1f5838af8bb4ee3a6c5d1b20468c32f3
Mau-Me <mauriciosm95@gmail.com>
2022-04-16 20:08:01 -0300

    Add: Rotas post, put e delete de usuarios
    Co-authored-by: tainahcarvalho21 <tainah_carvalho@hotmail.com>

--- src/controllers/usuarios.js
+++ src/controllers/usuarios.js
@@ -10,4 +10,23 @@ module.exports = (app) => {
 
     Usuario.buscaPorId(id, res);
   });
+
+  app.post("/usuarios", (req, res) => {
+    const usuario = req.body;
+
+    Usuario.adiciona(usuario, res);
+  });
+
+  app.put("/usuarios/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    const usuarioNovo = req.body;
+
+    Usuario.atualiza(id, usuarioNovo, res);
+  });
+
+  app.delete("/usuarios/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+
+    Usuario.deleta(id, res);
+  });
 };
