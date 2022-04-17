43f99e6803aa2d4955dd5b764c76ee0fc011e10d
Ge vasconcelos <geminivasconceelos@gmail.com>
2022-04-17 20:20:31 -0300

    Feat - criacao dos arquivos sql  criarDbLas, criarTabelaUsuario, InsereUsuario

---
+++ sql/insereUsuario.sql
@@ -0,0 +1,5 @@
+USE las;
+
+INSERT INTO usuarios(nome, urlFotoPerfil) VALUES('Gemini Vasconcelos', 'https://randomuser.me/api/portraits/men/91.jpg'), ('Sabrina Pureza', 'https://randomuser.me/api/portraits/women/62.jpg');
+
+
