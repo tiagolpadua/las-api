7c1960dd29f9e7b6261658a37093ad06b04e353b
Leandro <leogrcel@gmail.com>
2022-04-17 15:59:43 -0300

    Adicionando tabela usuarios.
    Co-authored-by: Joao joao.marcelo.c17@gmail.com

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const customExpress = require("./config/customExpress");
 const app = customExpress();
 const conexao = require("./infraestrutura/conexao");
-//const Tabelas = require("./infraestrutura/tabelas");
+const Tabelas = require("./infraestrutura/tabelas");
 const port = 3000;
 
 conexao.connect((erro) => {
@@ -9,7 +9,7 @@ conexao.connect((erro) => {
     console.log(erro);
   } else {
     console.log("Conectado com Sucesso!");
-    //Tabelas.init(conexao);
+    Tabelas.init(conexao);
     app.listen(port, () => console.log("Servidor Rodando na porta 3000"));
   }
 });
---
+++ src/infraestrutura/tabelas.js
@@ -0,0 +1,21 @@
+class Tabelas {
+  init(conexao) {
+    this.conexao = conexao;
+    this.criarUsuarios();
+  }
+
+  criarUsuarios() {
+    const sql =
+      "CREATE TABLE IF NOT EXISTS usuarios (id int AUTO_INCREMENT, nome varchar(50) NOT NULL, urlFotoPerfil varchar(100), PRIMARY KEY(id))";
+
+    this.conexao.query(sql, (erro) => {
+      if (erro) {
+        console.log(erro);
+      } else {
+        console.log("Tabela usuários criada com sucesso");
+      }
+    });
+  }
+}
+
+module.exports = new Tabelas();
