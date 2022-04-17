cb53f6210b82d3a01d854c69accb24286783ebfb
Mau-Me <mauriciosm95@gmail.com>
2022-04-17 14:14:20 -0300

    Add: Func validarURLFotoPerfil
    Co-authored-by: tainahcarvalho21 <tainah_carvalho@hotmail.com>

--- src/models/usuarios.js
+++ src/models/usuarios.js
@@ -1,4 +1,5 @@
 const conexao = require("../infraestrutura/conexao");
+const fetch = require("node-fetch");
 
 class Usuario {
   buscaTodos(res) {
@@ -42,13 +43,17 @@ class Usuario {
   adiciona(usuario, res) {
     const sql = "INSERT INTO usuarios SET ?";
 
-    conexao.query(sql, usuario, (erro) => {
-      if (erro) {
-        res.status(400).json(erro);
-      } else {
-        res.status(201).json(usuario);
-      }
-    });
+    if (this.validarURLFotoPerfil(usuario.urlFotoPerfil)) {
+      conexao.query(sql, usuario, (erro) => {
+        if (erro) {
+          res.status(400).json(erro);
+        } else {
+          res.status(201).json(usuario);
+        }
+      });
+    } else {
+      res.status(400);
+    }
   }
 
   atualiza(id, usuarioNovo, res) {
@@ -73,6 +78,24 @@ class Usuario {
         res.status(200).json({ id });
       }
     });
+  }
+
+  async validarURLFotoPerfil(url) {
+    try {
+      const expressao =
+        /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/gi;
+
+      const regex = new RegExp(expressao);
+
+      if (url.match(regex)) {
+        const res = await fetch(url);
+        return res.status == 200 ? true : false;
+      } else {
+        return false;
+      }
+    } catch (error) {
+      return false;
+    }
   }
 }
 
