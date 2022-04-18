948745ab99c344157fa50f48c621eb56bf3a40aa
Igor Ferreira <48239402+imagno@users.noreply.github.com>
2022-04-18 09:02:33 -0300

    configurando express e consign

---
+++ config/customExpress.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const consign = require("consign");
+
+module.exports = () => {
+  const app = express();
+
+  app.use(express.urlencoded());
+  app.use(express.json());
+
+  consign()
+    .include("controllers")
+    .into(app);
+  return app;
+};
