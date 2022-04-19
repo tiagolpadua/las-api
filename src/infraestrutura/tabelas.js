c6b8edd93d3f047108f9bb8ad7a68d8f78facd58
Lucas Lima <62082334+L1malucas@users.noreply.github.com>
2022-04-19 17:33:14 -0300

    Update tabelas.js

--- src/infraestrutura/tabelas.js
+++ src/infraestrutura/tabelas.js
@@ -2,10 +2,10 @@ class Tabelas {
   init(conexao) {
     this.conexao = conexao;
 
-    this.criaUsuarios();
+    this.criarUsuarios();
   }
 
-  criaUsuarios() {
+  criarUsuarios() {
     const sql = `CREATE TABLE IF NOT EXISTS Usuarios
       (id INT AUTO_INCREMENT PRIMARY KEY,
       nome VARCHAR(100) NOT NULL,
