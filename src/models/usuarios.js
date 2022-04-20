06ddaf2d8b630ed8450b9997aadcce385a70c421
CatiaLarissa <catialarissaramos@gmail.com>
2022-04-20 12:47:06 -0300

    enviando arquivos

---
+++ src/models/usuarios.js
@@ -0,0 +1,83 @@
+const conexao = require("../infraestrutura/conexao");
+
+class Usuario {
+  adiciona(usuario, res) {
+    const usuarioEhValido = usuario.nome.length >= 5;
+
+    const validacoes = [
+      {
+        nome: "usuario",
+        valido: usuarioEhValido,
+        mensagem: "usuario deve ter pelo menos cinco caracteres",
+      },
+    ];
+
+    const erros = validacoes.filter((campo) => !campo.valido);
+    const existemErros = erros.length;
+
+    if (existemErros) {
+      res.status(400).json(erros);
+    } else {
+      const sql = "INSERT INTO usuarios SET ?";
+
+      conexao.query(sql, (erro) => {
+        if (erro) {
+          res.status(400).json(erro);
+        } else {
+          res.status(201).json(usuario);
+        }
+      });
+    }
+  }
+
+  lista(res) {
+    const sql = "SELECT * FROM Usuarios";
+
+    conexao.query(sql, (erro, resultados) => {
+      if (erro) {
+        res.status(400).json(erro);
+      } else {
+        res.status(200).json(resultados);
+      }
+    });
+  }
+
+  buscaPorId(id, res) {
+    const sql = `SELECT * FROM Usuarios WHERE id=${id}`;
+
+    conexao.query(sql, (erro, resultados) => {
+      const usuario = resultados[0];
+      if (erro) {
+        res.status(400).json(erro);
+      } else {
+        res.status(200).json(usuario);
+      }
+    });
+  }
+
+  altera(id, valores, res) {
+    const sql = "UPDATE Usuarios SET ? WHERE id=?";
+
+    conexao.query(sql, [valores, id], (erro) => {
+      if (erro) {
+        res.status(400).json(erro);
+      } else {
+        res.status(200).json({ ...valores, id });
+      }
+    });
+  }
+
+  deleta(id, res) {
+    const sql = "DELETE FROM Usuarios WHERE id=?";
+
+    conexao.query(sql, id, (erro) => {
+      if (erro) {
+        res.status(400).json(erro);
+      } else {
+        res.status(200).json({ id });
+      }
+    });
+  }
+}
+
+module.exports = new Usuario();
