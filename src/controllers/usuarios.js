061df65712790477229916307c389f80a97dc33b
amandassa <amandassa504@gmail.com>
2022-04-21 19:40:45 -0300

    Update em usuarios: metodos e endpoints

--- src/controllers/usuarios.js
+++ src/controllers/usuarios.js
@@ -1,9 +1,18 @@
 const Usuario = require("../models/usuarios");
 
 module.exports = (app) => {
+    app.get("/usuarios", (res) => {
+        Usuario.listar(res);
+    });
+
     app.get("/usuarios/:id", (req, res) => {
         const id = parseInt(req.params.id);
         Usuario.buscaPorId(id, res);
+    });
+
+    app.get("/usuarios/nome/:nome", (req, res) => {
+        const nome = req.params.nome;
+        Usuario.buscaPorNome(nome, res);
     });
 
     app.post("/usuarios", (req, res) => {
@@ -11,8 +20,14 @@ module.exports = (app) => {
         Usuario.add(usuario, res);
     });
 
-    app.get("/usuarios/nome/:nome", (req, res) => {
-        const nome = req.params.nome;
-        Usuario.buscaPorNome(nome, res);
+    app.put("/usuarios/:id", (req, res) => {
+        const id = req.id;
+        const valores = req.body;
+        Usuario.alterarUsuario(id, valores, res);
+    });
+
+    app.delete("/usuarios/:id", (req, res) => {
+        const id = req.id;
+        Usuario.excluirUsuario(id, res);
     });
 };
--- src/models/usuarios.js
+++ src/models/usuarios.js
@@ -1,7 +1,18 @@
+const res = require("express/lib/response");
 const conexao = require("../infraestrutura/conexao");
 
 
 class Usuario {
+    listar (res) {
+        const query = "SELECT * FROM usuarios";
+        conexao.query(query, (err, resultados) => {
+            if (err) {
+                res.status(400).json(err);
+            } else {
+                res.status(200).json(resultados);
+            }
+        });
+    }
     buscaPorId(id, res) {
         const sql = `SELECT * FROM usuarios WHERE ID=${id}`;
         conexao.query(sql, (erro, result) => {
@@ -22,9 +33,23 @@ class Usuario {
         } else res.status(400).json("Usuario inválido.");
     }
     buscaPorNome(nome, res) {
-        const sql = `SELECT * FROM usuarios WHERE NOME LIKE "${nome}%"`;
+        const sql = `SELECT * FROM usuarios WHERE nome LIKE "${nome}%"`;
         conexao.query(sql, (erro, result) => {
             if (erro) res.status(400).json(erro);
+            else res.status(200).json(result);
+        });
+    }
+    alterarUsuario(id, valores, res) {
+        const sql = "UPDATE usuarios SET ? WHERE id = ?";
+        conexao.query(sql, valores, id, (err, result) => {
+            if (err) res.status(400).json(err);
+            else res.status(200).json(result);
+        });
+    }
+    excluirUsuario(id) {
+        const sql = "DELETE FROM usuarios WHERE id = ?";
+        conexao.query(sql, id, (err, result) => {
+            if (err) res.status(400).json(err);
             else res.status(200).json(result);
         });
     }
