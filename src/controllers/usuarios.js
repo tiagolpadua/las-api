1985a7dc2a1416d6be526d45e54d880bee60bade
alef nunes <alefnunes.dev@gmail.com>
2022-04-25 18:58:33 -0300

    Semana 17 - Commit 3

--- src/controllers/usuarios.js
+++ src/controllers/usuarios.js
@@ -1,31 +1,37 @@
 const Usuario = require("../models/usuarios");
 
 module.exports = (app) => {
-    app.get("/usuarios/:id", (req, res) => {
-        const id = req.params.id;
+  app.get("/usuarios/:id", (req, res) => {
+    const id = parseInt(req.params.id);
 
-        Usuario.buscarUsuario(id, res);
-    });
+    Usuario.buscarUsuario(id, res);
+  });
 
-    app.post("/usuarios", (req, res) => {
-        const usuario = req.body;
+  app.post("/usuarios", (req, res) => {
+    const newUser = req.body;
 
-        return res.json(usuario);
-    });
+    Usuario.incluirUsuario(newUser, res);
+  });
 
-    app.put("/usuarios/:id", (req, res) => {
-        const usuario = req.body;
+  app.put("/usuarios/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    const modificacao = req.body;
 
-        return res.json(usuario);
-    });
+    Usuario.alterarUsuario(id, modificacao, res);
+  });
 
-    app.get("/usuarios", (req, res) => {
-        res.send("Lista de Usuarios");
-    });
+  app.get("/usuarios", (req, res) => {
+    Usuario.listarUsuarios(res);
+  });
 
-    app.delete("/usuarios/:id", (req, res) => {
-        const usuario = req.params;
+  app.delete("/usuarios/:id", (req, res) => {
+    const id = parseInt(req.params.id);
 
-        return res.json(usuario);
-    });
+    Usuario.deletarUsuario(id, res);
+  });
+  app.get("/usuarios/nome/:nome", (req, res) => {
+    const nome = req.params.nome;
+
+    Usuario.buscarUsuarioNome(nome, res);
+  });
 };
--- src/models/usuarios.js
+++ src/models/usuarios.js
@@ -1,20 +1,91 @@
 const conexao = require("../infraestrutura/conexao");
 
 class Usuario {
-    buscarUsuario(id, res) {
-        const sql = `SELECT * FROM las.usuario WHERE id=?`;
+  buscarUsuario(id, res) {
+    const sql = "SELECT * FROM las.usuario WHERE id = ?";
 
-        conexao.query(sql, id, (erro, resultados) => {
-            const retornoUsuario = resultados[0];
-            if (!erro && !retornoUsuario) {
-                res.status(404).end();
-            } else if (erro) {
-                res.status(400).json(erro);
-            } else {
-                res.status(200).json(retornoUsuario);
-            }
-        });
-    }
-}
+    conexao.query(sql, id, (erro, results) => {
+      const retornoUsuario = results[0];
+
+      if (erro) {
+        res.status(400).json({ erro: erro });
+      } else {
+        res.status(200).json(retornoUsuario);
+      }
+    });
+  }
+  incluirUsuario(newUser, res) {
+    const sqlIsert = "INSERT INTO las.usuario SET ?";
+
+    conexao.query(sqlIsert, newUser, (erro) => {
+      if (erro) {
+        res.status(400).json({ erro: erro });
+      } else {
+        res.status(200).json(newUser);
+      }
+    });
+  }
+  alterarUsuario(id, modificacao, res) {
+    const sqlUpdate = "UPDATE las.usuario SET ? WHERE id = ?";
+
+    conexao.query(sqlUpdate, [modificacao, id], (erro) => {
+      if (erro) {
+        res.status(400).json({ erro: erro });
+      } else {
+        res.status(200).json(modificacao);
+      }
+    });
+  };
+  listarUsuarios(res) {
+    const sqlListar = "SELECT * FROM las.usuario";
+
+    conexao.query(sqlListar, (erro, resultados) => {
+      if (erro) {
+        res.status(400).json({ err: erro });
+      } else {
+        res.status(200).json(resultados);
+      }
+    });
+  };
+  deletarUsuario(id, res) {
+    const sqlDeletar = "DELETE las.usuario SET ? WHERE id = ?";
+
+    conexao.query(sqlDeletar, id, (erro, resultados) => {
+      if (erro) {
+        res.status(400).json({ erro: erro });
+      } else {
+        res.status(200).json(resultados);
+      }
+    });
+  };
+  buscarUsuarioNome(nome, res) {
+    const sqlNome = "SELECT * FROM las.usuario WHERE nome = ?";
+
+    conexao.query(sqlNome, nome, (erro, resultado) => {
+      const usuario = resultado[0];
+
+      if (erro) {
+        res.status(400).json({ erro: erro });
+      } else {
+        res.status(200).json(usuario);
+      }
+    });
+  };
+
+
+validarNomeUsuarioNaoUtilizado(nome) {
+  const sql = "SELECT * FROM usuarios WHERE nome = ?";
+
+  return new Promise((resolve, reject) => {
+    conexao.query(sql, nome, (erro, resultado) => {
+      if (erro) {
+        reject(erro);
+      } else {
+        resolve(resultado.length > 0 ? false : true);
+      }
+    });
+  });
+  };
+};
 
 module.exports = new Usuario();
