58a84d99bc5721e538b79e85c139a9558f5a3f33
Mau-Me <mauriciosm95@gmail.com>
2022-05-04 23:19:16 -0300

    Refactor: Utiliza o design pattern repository
    Co-authored-by: tainahcarvalho21 <tainah_carvalho@hotmail.com>

--- src/models/usuarios.js
+++ src/models/usuarios.js
@@ -1,16 +1,10 @@
-const pool = require("../infraestrutura/conexao");
+const pool = require("../infraestrutura/database/conexao");
+const repositorio = require("../repositorio/usuario");
 const fetch = require("node-fetch");
 
 class Usuarios {
-  listar(res, next) {
-    const sql = "SELECT * FROM Usuarios";
-    pool.query(sql, (erro, resultados) => {
-      if (erro) {
-        next(erro);
-      } else {
-        res.status(200).json(resultados);
-      }
-    });
+  listar() {
+    return repositorio.listar();
   }
 
   buscarPorId(id, res, next) {
