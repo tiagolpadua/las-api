0fc107f7efe99bc3b24612eb36de4d95abaad7a1
Mau-Me <mauriciosm95@gmail.com>
2022-05-05 03:23:42 -0300

    Add: Rotas post, put e delete de tiposVendas
    Co-authored-by: tainahcarvalho21 <tainah_carvalho@hotmail.com>

--- src/models/tiposVendas.js
+++ src/models/tiposVendas.js
@@ -4,6 +4,22 @@ class TiposVendas {
   listar() {
     return repositorio.listar();
   }
+
+  adicionar(evento) {
+    return repositorio.adicionar(evento);
+  }
+
+  buscaPorId(id) {
+    return repositorio.buscaPorId(id);
+  }
+
+  alterar(id, valores) {
+    return repositorio.alterar(id, valores);
+  }
+
+  excluir(id) {
+    return repositorio.excluir(id);
+  }
 }
 
 module.exports = new TiposVendas();
--- src/repositorio/tiposVendas.js
+++ src/repositorio/tiposVendas.js
@@ -5,6 +5,26 @@ class TiposVendas {
     const sql = "SELECT * FROM TiposVendas";
     return query(sql);
   }
+
+  adicionar(tipoVenda) {
+    const sql = "INSERT INTO TiposVendas SET ?";
+    return query(sql, tipoVenda);
+  }
+
+  buscaPorId(id) {
+    const sql = "SELECT * FROM TiposVendas WHERE id = ?";
+    return query(sql, id);
+  }
+
+  alterar(id, tipoVendaAtualizada) {
+    const sql = "UPDATE TiposVendas SET ? WHERE id = ?";
+    return query(sql, [tipoVendaAtualizada, id]);
+  }
+
+  excluir(id) {
+    const sql = "DELETE FROM TiposVendas WHERE id = ?";
+    return query(sql, id);
+  }
 }
 
 module.exports = new TiposVendas();
