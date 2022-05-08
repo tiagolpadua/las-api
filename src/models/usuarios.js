3ebcc198e111bfa856d55b01fc04dfa184566880
amandassa <amandassa504@gmail.com>
2022-05-07 23:35:06 -0300

    inicio -- pattern repository

--- src/models/usuarios.js
+++ src/models/usuarios.js
@@ -1,16 +1,10 @@
 const pool = require("../infraestrutura/conexao");
 const fetch = require("node-fetch");
+const repositorio = require("../repositorios/usuario");
 
 class Usuarios {
-  listar(res, next) {
-    const sql = "SELECT * FROM Usuarios";
-    pool.query(sql, (erro, resultados) => {
-      if (erro) {
-        next(erro);
-      } else {
-        res.status(200).json(resultados);
-      }
-    });
+  listar() {
+    return repositorio.listar();
   }
 
   buscarPorId(id, res, next) {
