94c18a0194fc1d1b39b5e0fb04e4a34f9588e46e
Renata Santana <renatasantana114@gmail.com>
2022-05-08 15:15:46 -0300

    Complementando os desejáveis

--- src/controllers/tiposVendas.js
+++ src/controllers/tiposVendas.js
@@ -6,4 +6,29 @@ module.exports = (app) => {
       .then((resultados) => res.status(201).json(resultados))
       .catch((erros) => res.status(400).json(erros))
   );
+
+  app.post("/tipos-vendas", (req, res) => {
+    const TiposVendas = req.body;
+
+    TiposVendas.incluir(TiposVendas)
+      .then((resultados) => res.status(201).json(resultados))
+      .catch((erros) => res.status(400).json(erros));
+  });
+
+  app.put("/tipos-vendas/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    const valores = req.body;
+
+    TiposVendas.alterar(id, valores)
+      .then((resultados) => res.status(201).json(resultados))
+      .catch((erros) => res.status(400).json(erros));
+  });
+
+  app.delete("/tipos-vendas/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+
+    TiposVendas.excluir(id)
+      .then((resultados) => res.status(201).json(resultados))
+      .catch((erros) => res.status(400).json(erros));
+  });
 };
--- src/models/tiposVendas.js
+++ src/models/tiposVendas.js
@@ -4,6 +4,18 @@ class TiposVendas {
   listar() {
     return repositorio.listarTiposVendas().then((resultados) => resultados);
   }
+
+  incluir(TiposVendas) {
+    return repositorio.incluirTiposVendas(TiposVendas).then((TiposVendas) => TiposVendas);
+  }
+
+  alterar(id, valores) {
+    return repositorio.alterarTiposVendas(id, valores).then((TiposVendas) => TiposVendas);
+  }
+
+  excluir(id) {
+    return repositorio.excluirTiposVendas(id);
+  }
 }
 
 module.exports = new TiposVendas();
