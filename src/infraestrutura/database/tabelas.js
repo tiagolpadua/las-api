53cb5e88e60175167aef4d7a7860e9865e67859c
Leticia Ribeiro <letsribeiro2@gmail.com>
2022-05-11 18:17:17 -0300

    improve readability

--- src/infraestrutura/database/tabelas.js
+++ src/infraestrutura/database/tabelas.js
@@ -9,7 +9,11 @@ class Tabelas {
 
   criarUsuarios() {
     const sql =
-      "CREATE TABLE IF NOT EXISTS Usuarios(id INT AUTO_INCREMENT NOT NULL, nome varchar(100) NOT NULL, urlFotoPerfil text, UNIQUE (nome), PRIMARY KEY(id))";
+      `CREATE TABLE IF NOT EXISTS Usuarios(
+        id INT AUTO_INCREMENT NOT NULL,
+        nome varchar(100) NOT NULL,
+        urlFotoPerfil text,
+        UNIQUE (nome), PRIMARY KEY(id))`;
 
     this.pool.query(sql, (erro) => {
       if (erro) {
@@ -22,7 +26,16 @@ class Tabelas {
 
   criarEventos() {
     const sql =
-      "CREATE TABLE IF NOT EXISTS Eventos(id INT AUTO_INCREMENT NOT NULL, nome varchar(100) NOT NULL, descricao text NOT NULL, urlFoto text NOT NULL, dataInicio date NOT NULL, dataFim date NULL, status varchar(20), PRIMARY KEY(id))";
+      `CREATE TABLE IF NOT EXISTS Eventos(
+        id INT AUTO_INCREMENT NOT NULL,
+        nome varchar(100) NOT NULL,
+        descricao text NOT NULL,
+        urlFoto text NOT NULL,
+        dataInicio date NOT NULL,
+        dataFim date NOT NULL,
+        status varchar(20),
+        PRIMARY KEY(id))`;
+
     this.pool.query(sql, (erro) => {
       if (erro) {
         console.log(erro);
@@ -34,7 +47,11 @@ class Tabelas {
 
 
   criarTipoVendas() {
-    const sql = "CREATE TABLE IF NOT EXISTS tipoVendas (id INT NOT NULL, descricao text NOT NULL)";
+    const sql =
+     `CREATE TABLE IF NOT EXISTS tipoVendas(
+        id INT NOT NULL,
+        descricao text NOT NULL)`;
+        
     this.pool.query(sql, (erro) => {
       if (erro) console.log(erro);
       else console.log("Tabela tipoVendas criada com sucesso");
