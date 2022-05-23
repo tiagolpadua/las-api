6b9ea7a9de471aed39998bb5cac0e1a33a67803f
Mau-Me <mauriciosm95@gmail.com>
2022-05-23 13:21:22 -0300

    Add: Rota get/put para dados pessoais do usuário
    Co-authored-by: tainahcarvalho21 <tainah_carvalho@hotmail.com>

--- src/controllers/usuarios.js
+++ src/controllers/usuarios.js
@@ -10,15 +10,15 @@ module.exports = (app) => {
   app.get("/usuarios/:id", (req, res) => {
     const id = parseInt(req.params.id);
     Usuarios.buscarPorId(id)
-      .then((resultados) => res.json(resultados))
-      .catch((erros) => res.status(400).json(erros));
+      .then((usuario) => (usuario ? res.json(usuario) : res.status(404).send()))
+      .catch((erros) => res.status(404).json(erros));
   });
 
   app.post("/usuarios", (req, res) => {
     const usuarios = req.body;
     Usuarios.adicionar(usuarios)
-      .then((resultados) => res.json({ id: resultados.insertId, ...usuarios }))
-      .catch((erros) => res.status(400).json(erros));
+      .then((resultados) => res.status(201).json(resultados))
+      .catch((erros) => res.status(404).json(erros));
   });
 
   app.put("/usuarios/:id", (req, res) => {
@@ -26,20 +26,37 @@ module.exports = (app) => {
     const valores = req.body;
     Usuarios.alterar(id, valores)
       .then(() => res.json({ id, ...valores }))
-      .catch((erros) => res.status(400).json(erros));
+      .catch((erros) => res.status(404).json(erros));
   });
 
   app.delete("/usuarios/:id", (req, res) => {
     const id = parseInt(req.params.id);
     Usuarios.excluir(id)
       .then(() => res.json({ id }))
-      .catch((erros) => res.status(400).json(erros));
+      .catch((erros) => res.status(404).json(erros));
   });
 
   app.get("/usuarios/nome/:nome", (req, res) => {
     const nome = req.params.nome;
     Usuarios.buscarPorNome(nome)
       .then((resultados) => res.json(resultados))
-      .catch((erros) => res.status(400).json(erros));
+      .catch((erros) => res.status(404).json(erros));
+  });
+
+  // DADOS PESSOAIS
+
+  app.get("/usuarios/:usuarioId/dados-pessoais", (req, res) => {
+    const id = req.params.usuarioId;
+    Usuarios.buscarDadosPessoais(id)
+      .then((resultado) => res.json(resultado))
+      .catch((erros) => res.status(402).json(erros));
+  });
+
+  app.put("/usuarios/:usuarioId/dados-pessoais", (req, res) => {
+    const id = req.params.usuarioId;
+    const valores = req.body;
+    Usuarios.atualizarDadosPessoais(id, valores)
+      .then((resultado) => res.json(resultado))
+      .catch((erros) => res.status(402).json(erros));
   });
 };
