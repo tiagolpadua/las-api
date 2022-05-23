d86f387e2b8e2d099b079e29560c2d30f58d2297
Mau-Me <mauriciosm95@gmail.com>
2022-05-23 13:26:58 -0300

    Refactor: Especifica campos de consulta / Add: Campos dados pessoais
    Co-authored-by: tainahcarvalho21 <tainah_carvalho@hotmail.com>

--- src/repositorio/usuario.js
+++ src/repositorio/usuario.js
@@ -2,7 +2,7 @@ const query = require("../infraestrutura/database/queries");
 
 class Usuario {
   listar() {
-    const sql = "SELECT * FROM Usuarios";
+    const sql = "SELECT id, nome, urlFotoPerfil FROM Usuarios";
     return query(sql);
   }
 
@@ -11,9 +11,10 @@ class Usuario {
     return query(sql, usuario);
   }
 
-  buscaPorId(id) {
-    const sql = "SELECT * FROM Usuarios WHERE id = ?";
-    return query(sql, id);
+  async buscaPorId(id) {
+    const sql = "SELECT id, nome, urlFotoPerfil FROM Usuarios WHERE id = ?";
+    const resultados = await query(sql, id);
+    return resultados[0];
   }
 
   alterar(id, usuarioAtualizado) {
@@ -28,8 +29,36 @@ class Usuario {
 
   buscarPorNome(nome) {
     nome = "%" + nome + "%";
-    const sql = "SELECT * FROM Usuarios WHERE nome like ?";
+    const sql =
+      "SELECT id, nome, urlFotoPerfil FROM Usuarios WHERE nome like ?";
     return query(sql, nome);
+  }
+
+  buscarDadosPessoais(id) {
+    const sql =
+      "SELECT nomeCompleto, dataNascimento, rg, cpf FROM Usuarios WHERE id = ?";
+    return query(sql, id);
+  }
+
+  atualizarDadosPessoais(id, dadosPessoais) {
+    const sql = "UPDATE Usuarios SET ? WHERE id = ?";
+    return query(sql, [dadosPessoais, id]);
+  }
+
+  async isNomeUsuarioUtilizado(nome) {
+    const sql = "SELECT * FROM Usuarios WHERE nome = ?";
+
+    query(sql, nome, (erro, resultados) => {
+      if (erro) {
+        throw erro;
+      } else {
+        if (resultados.length > 0) {
+          return true;
+        } else {
+          return false;
+        }
+      }
+    });
   }
 }
 
