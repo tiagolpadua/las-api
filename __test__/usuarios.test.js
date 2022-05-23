e349a100e671e45705539d3980417153a2aa9525
Mau-Me <mauriciosm95@gmail.com>
2022-05-23 17:50:58 -0300

    Add: Testes das rotas get e put de dados-pessoais e put e delete de usuarios
    Co-authored-by: tainahcarvalho21 <tainah_carvalho@hotmail.com>

--- __test__/usuarios.test.js
+++ __test__/usuarios.test.js
@@ -107,7 +107,7 @@ describe("API de Usuários", () => {
   test("Inserir um Usuário com Nome e URL Inválidos", async () => {
     const usuario = {
       nome: "",
-      urlFotoPerfil: "https://urlInvalida.com.br",
+      urlFotoPerfil: "xxxxxxxxxxxxxxxxx",
     };
     const resp = await request.post("/usuarios").send(usuario);
 
@@ -124,5 +124,92 @@ describe("API de Usuários", () => {
         valido: false,
       },
     ]);
+  });
+
+  test("Deletar Usuários com ID Existente", async () => {
+    const resp = await request.delete("/usuarios/1");
+    expect(resp.statusCode).toBe(204);
+  });
+
+  test("Deletar Usuários com ID Inexistente", async () => {
+    const resp = await request.delete("/usuarios/99");
+    expect(resp.statusCode).toBe(404);
+  });
+
+  test("Atualizar um Usuário com Id Existente com os Dados Válidos", async () => {
+    const usuario = {
+      nome: "Aleatório",
+      urlFotoPerfil: "https://randomuser.me/api/portraits/men/16.jpg",
+    };
+    const resp = await request.put("/usuarios/3").send(usuario);
+
+    expect(resp.statusCode).toBe(200);
+    expect(resp.body).toEqual({ id: 3, ...usuario });
+  });
+
+  test("Atualizar um Usuário com Id inexistente", async () => {
+    const usuario = {
+      nome: "Aleatório",
+      urlFotoPerfil: "https://randomuser.me/api/portraits/men/16.jpg",
+    };
+    const resp = await request.put("/usuarios/999").send(usuario);
+
+    expect(resp.statusCode).toBe(404);
+  });
+
+  test("Buscar Usuários por Nome Existente", async () => {
+    const resp = await request.get("/usuarios/nome/Mauricio");
+    const usuario = [
+      {
+        id: 2,
+        nome: "Mauricio Menezes",
+        urlFotoPerfil: "https://avatars.githubusercontent.com/u/20570844?v=4",
+      },
+    ];
+
+    expect(resp.statusCode).toBe(200);
+    expect(resp.body).toEqual(usuario);
+  });
+
+  test("Buscar Usuários por Nome Inexistente", async () => {
+    const resp = await request.get("/usuarios/nome/pedro");
+
+    expect(resp.statusCode).toBe(200);
+    expect(resp.body).toEqual([]);
+  });
+
+  test("Buscar Dados Pessoais do Usuários com Id Existente", async () => {
+    const resp = await request.get("/usuarios/2/dados-pessoais");
+    const dadosPessoaisUsuario = {
+      nomeCompleto: "Mauricio Souza Menezes",
+      dataNascimento: "1995-07-30",
+      rg: "9999999999",
+      cpf: "99999999999",
+    };
+
+    expect(resp.statusCode).toBe(200);
+    expect(resp.body).toEqual(dadosPessoaisUsuario);
+  });
+
+  test("Buscar Dados Pessoais do Usuários com Id Inexistente", async () => {
+    const resp = await request.get("/usuarios/99/dados-pessoais");
+
+    expect(resp.statusCode).toBe(200);
+    expect(resp.body).toEqual([]);
+  });
+
+  test("Atualizar Dados Pessoais de um Usuário com Id Existente", async () => {
+    const dadosPessoaisUsuario = {
+      nomeCompleto: "Mauricio Menezes",
+      dataNascimento: "1995-07-30",
+      rg: "9999999999",
+      cpf: "99999999999",
+    };
+    const resp = await request
+      .put("/usuarios/2/dados-pessoais")
+      .send(dadosPessoaisUsuario);
+
+    expect(resp.statusCode).toBe(200);
+    expect(resp.body).toEqual(dadosPessoaisUsuario);
   });
 });
