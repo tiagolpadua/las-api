2f0012b787de5d693d36bac74daf27179210c25b
Leticia Ribeiro <letsribeiro2@gmail.com>
2022-05-29 21:32:33 -0300

    fix mock archive name

--- __tests__/usuarios.test.js
+++ __tests__/usuarios.test.js
@@ -8,22 +8,22 @@ describe("API de usuários", () => {
   test("Listar usuários", async () => {
     const resp = await request.get("/usuarios");
     expect(resp.statusCode).toBe(200);
-    expect(resp.body).toBe([
+    expect(resp.body).toStrictEqual([
       {
-        "nomeCompleto": "user1",
-        "urlFotoPerfil": "https://randomuser.me/api/portraits/women/75.jpg",
-        "email": "email1"
+          "nomeCompleto": "user1",
+          "urlFotoPerfil": "https://randomuser.me/api/portraits/women/75.jpg",
+          "email": "email1"
       },
       {
-        "nomeCompleto": "user2",
-        "urlFotoPerfil": "https://randomuser.me/api/portraits/women/74.jpg",
-        "email": "email2"
+          "nomeCompleto": "user2",
+          "urlFotoPerfil": "https://randomuser.me/api/portraits/women/74.jpg",
+          "email": "email2"
       },
       {
-        "nomeCompleto": "user3",
-        "urlFotoPerfil": "https://randomuser.me/api/portraits/women/90.jpg",
-        "email": "email3"
+          "nomeCompleto": "user3",
+          "urlFotoPerfil": "https://randomuser.me/api/portraits/women/90.jpg",
+          "email": "email3"
       }
-    ]);
+  ]);
   });
 });
