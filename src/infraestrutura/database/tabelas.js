70c20c980701df5a393aff79a8677354bf6e2cdc
Ge Vasconcelos <geminivasconceelos@gmail.com>
2022-05-30 16:21:02 -0300

    feat: criacao das tabelas criarUFs e criarMunicipios

--- src/infraestrutura/database/tabelas.js
+++ src/infraestrutura/database/tabelas.js
@@ -5,6 +5,8 @@ class Tabelas {
     this.criarUsuarios();
     this.criarEventos();
     this.criarTiposVendas();
+    this.criarUFs();
+    this.criarMunicipios();
   }
 
   criarUsuarios() {
@@ -62,6 +64,32 @@ class Tabelas {
 
     this.pool.query(sql, (erro) => {
       console.log(erro ? erro : "Tabela TipoVendas criada com sucesso");
+    });
+  }
+
+  criarUFs() {
+    const sql =
+      "CREATE TABLE IF NOT EXISTS UFs(id INT NOT NULL, sigla varchar(2) NOT NULL, nome varchar(50), primary key(id))";
+
+    this.pool.query(sql, (erro) => {
+      if (erro) {
+        console.log(erro);
+      } else {
+        console.log("Tabela UFs criada com sucesso");
+      }
+    });
+  }
+
+  criarMunicipios() {
+    const sql =
+      "CREATE TABLE IF NOT EXISTS Municipios(id INT NOT NULL, nome varchar(75) NOT NULL, uf_id int NOT NULL, uf_sigla varchar(2) NOT NULL, uf_nome varchar(30) NOT NULL, regiao_id int NOT NULL, regiao_sigla varchar(2) NOT NULL, regiao_nome varchar(20) NOT NULL , primary key(id))";
+
+    this.pool.query(sql, (erro) => {
+      if (erro) {
+        console.log(erro);
+      } else {
+        console.log("Tabela Municipio criada com sucesso");
+      }
     });
   }
 }
