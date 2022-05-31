669992a83ea508240331f622632c8eab32f98e7c
CatiaLarissa <catialarissaramos@gmail.com>
2022-05-31 11:00:15 -0300

    nova tentativa

--- __tests__/example.test.js
+++
@@ -1,8 +0,0 @@
-const customExpress = require("../src/config/customExpress");
-
-describe("Home", () => {
-  test("URL Base", async () => {
-    const app = customExpress();
-    expect(app).toBeDefined();
-  });
-});
