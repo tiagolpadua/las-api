0a9c7a612978f961efbb937353ebcc55ce1ffdeb
Leticia Ribeiro <letsribeiro2@gmail.com>
2022-05-30 21:39:10 -0300

    update uf controller

--- src/controllers/UFs.js
+++ src/controllers/UFs.js
@@ -9,7 +9,6 @@ module.exports = (app) => {
 
   app.get("/ufs/:uf/municipios", (req, res, next) => {
     const uf = req.params.uf;
-    console.log(uf);
     UFs.listarMunicipios(uf)
       .then((resultados) => res.status(200).json(resultados))
       .catch((erros) => next(erros));
